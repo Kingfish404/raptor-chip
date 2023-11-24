8c31cbb4bfe47a13c0e33ffb92006895d6bbe9ae
Yu Jin <jinyu929@qq.com>
2023-11-24 19:47:14 +0800

    feat: am klib

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,22 +5,24 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+#define SPRINT_BUF_SIZE 1024
+
+static char sprint_buf[SPRINT_BUF_SIZE];
+
 int printf(const char *fmt, ...)
 {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int n = vsprintf(sprint_buf, fmt, ap);
+  va_end(ap);
+  putstr(sprint_buf);
+  return n;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap)
 {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...)
-{
+  int buf_count = 0, ret = 0;
   char *pout = out, *p;
-  int d;
-  va_list ap;
-  va_start(ap, fmt);
   for (size_t i = 0; fmt[i] != '\0'; i++)
   {
     switch (fmt[i])
@@ -29,11 +31,13 @@ int sprintf(char *out, const char *fmt, ...)
       switch (fmt[++i])
       {
       case 'd':
-        d = va_arg(ap, int);
+      {
+        int d = va_arg(ap, int);
         if (d < 0)
         {
           *pout = '-';
           pout++;
+          buf_count++;
         }
         char *s_p = pout;
         do
@@ -41,6 +45,7 @@ int sprintf(char *out, const char *fmt, ...)
           int td = d % 10;
           *pout = '0' + (td >= 0 ? td : -td);
           pout++;
+          buf_count++;
           d /= 10;
         } while (d != 0);
         char *e_p = pout - 1;
@@ -52,23 +57,47 @@ int sprintf(char *out, const char *fmt, ...)
           s_p++;
           e_p--;
         }
-        break;
+      }
+      break;
       case 's':
+      {
         p = va_arg(ap, char *);
         strcpy(pout, p);
         pout += strlen(p);
-        break;
+      }
+      break;
+      case 'c':
+      {
+        char c = va_arg(ap, int);
+        *pout = c;
+        pout++;
+      }
+      break;
       }
       break;
     default:
       (*pout) = fmt[i];
       pout++;
+      buf_count++;
       break;
     }
     (*pout) = '\0';
+    if (buf_count + 1 == SPRINT_BUF_SIZE)
+    {
+      ret = -1;
+      break;
+    }
   }
+  return ret;
+}
+
+int sprintf(char *out, const char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
   va_end(ap);
-  return 0;
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...)
