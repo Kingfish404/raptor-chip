2f2a360ffd8345c80b655e117f76bfae916d711c
tracer-ysyx <tracer@ysyx.org>
2024-02-08 17:46:11 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 17:46  up 8 days, 1 hr, 7 users, load averages: 3.65 4.19 4.18

--- npc/include/verilog.h
+++ npc/include/verilog.h
@@ -16,10 +16,10 @@ static inline void verilog_connect(TOP_NAME *top, NPCState *npc)
   // for difftest
   npc->inst = (uint32_t *)&(CONCAT(VERILOG_PREFIX, __DOT__ifu__DOT__inst_ifu));
 
-  npc->gpr = (word_t *)&(top->rootp->ysyx__DOT__regs__DOT__rf);
-  npc->pc = (uint32_t *)&(top->rootp->ysyx__DOT__pc);
+  npc->gpr = (word_t *)&CONCAT(VERILOG_PREFIX, __DOT__regs__DOT__rf);
+  npc->pc = (uint32_t *)&CONCAT(VERILOG_PREFIX, __DOT__pc);
   npc->ret = npc->gpr + reg_str2idx("a0");
-  word_t *csr = (word_t *)&(top->rootp->ysyx__DOT__exu__DOT__csr__DOT__csr);
+  word_t *csr = (word_t *)&CONCAT(VERILOG_PREFIX, __DOT__exu__DOT__csr__DOT__csr);
 
   npc->state = NPC_RUNNING;
   npc->mstatus = csr + CSR_MSTATUS;
