41fb4f9812ca9a51a5bceb94ce0f80682aefb87c
tracer-ysyx <tracer@ysyx.org>
2024-02-08 17:01:30 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 17:01  up 8 days, 16 mins, 7 users, load averages: 3.82 2.98 2.91

---
+++ npc/ssrc/src/main/scala/Elaborate.scala
@@ -0,0 +1,8 @@
+import circt.stage._
+import gcd.GCD
+
+object Elaborate extends App {
+  def top = new GCD()
+  val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
+  (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+}
