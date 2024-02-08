b0896015c8689c6e5d480ae77f3f7546ba908313
tracer-ysyx <tracer@ysyx.org>
2024-02-08 18:47:38 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 18:47  up 8 days,  2:02, 7 users, load averages: 3.60 4.19 4.42

--- npc/vsrc/ysyx_bus.v
+++ npc/vsrc/ysyx_bus.v
@@ -151,7 +151,7 @@ module ysyx_BUS_ARBITER(
   wire ifsr_ready = `ysyx_IFSR_ENABLE ? lfsr[19] : 1;
   always @(posedge clk ) begin lfsr <= {lfsr[18:0], lfsr[19] ^ lfsr[18]}; end
 
-  assign io_master_araddr = araddr;
+  // assign io_master_araddr = araddr;
   assign io_master_arvalid = sram_arvalid;
   assign arready_o = io_master_arready;
 
