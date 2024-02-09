3e254b0428f71fac59dc2906392686faedf873e5
tracer-ysyx <tracer@ysyx.org>
2024-02-09 12:05:11 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 12:05  up 8 days, 19:19, 7 users, load averages: 4.45 3.66 3.67

--- npc/csrc/mem/memory.cc
+++ npc/csrc/mem/memory.cc
@@ -174,7 +174,7 @@ extern "C" void mrom_read(uint32_t addr, uint32_t *data)
     // printf("first byte = 0x%x\n", ramdisk_mrom_start[0]);
     // *data = 0b00000000000100000000000001110011; // ebreak
     uint32_t *mrom = (uint32_t *)(ramdisk_mrom_start + offset);
-    *data = mrom[offset];
+    *data = *mrom;
     printf("raddr: " FMT_WORD_NO_PREFIX ", data: " FMT_WORD_NO_PREFIX "\n",
            addr, *data);
 }
