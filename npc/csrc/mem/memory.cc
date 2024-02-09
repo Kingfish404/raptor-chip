e2af47a2a417ca8a0cbe67a4e6360909b33200f1
tracer-ysyx <tracer@ysyx.org>
2024-02-09 10:45:14 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 10:45  up 8 days, 17:59, 7 users, load averages: 2.17 2.46 3.03

--- npc/csrc/mem/memory.cc
+++ npc/csrc/mem/memory.cc
@@ -127,6 +127,31 @@ extern "C" void pmem_write(word_t waddr, word_t wdata, char wmask)
     }
 }
 
+/* Import a binary file */
+#define IMPORT_BIN(sect, file, sym) asm(                                    \
+    ".section " #sect "\n"                  /* Change section */            \
+    ".balign 4\n"                           /* Word alignment */            \
+    ".global " #sym "\n"                    /* Export the object address */ \
+    #sym ":\n"                              /* Define the object label */   \
+    ".incbin \"" file "\"\n"                /* Import the file */           \
+    ".global _sizeof_" #sym "\n"            /* Export the object size */    \
+    ".set _sizeof_" #sym ", . - " #sym "\n" /* Define the object size */    \
+    ".balign 4\n"                           /* Word alignment */            \
+    ".section \".text\"\n")                 /* Restore section */
+
+/* Import a part of binary file */
+#define IMPORT_BIN_PART(sect, file, ofs, siz, sym) asm( \
+    ".section " #sect "\n"                              \
+    ".balign 4\n"                                       \
+    ".global " #sym "\n" #sym ":\n"                     \
+    ".incbin \"" file "\"," #ofs "," #siz "\n"          \
+    ".global _sizeof_" #sym "\n"                        \
+    ".set _sizeof_" #sym ", . - " #sym "\n"             \
+    ".balign 4\n"                                       \
+    ".section \".text\"\n")
+
+extern const char _binary_foo_bin_start[], _binary_foo_bin_size[];
+
 extern "C" void flash_read(uint32_t addr, uint32_t *data)
 {
     *data = 0b00000000000100000000000001110011; // ebreak
