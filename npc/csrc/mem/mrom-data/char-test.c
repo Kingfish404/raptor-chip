2584342f222e553f8df1be15a471ed53a1f0016e
tracer-ysyx <tracer@ysyx.org>
2024-02-11 00:55:40 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64  0:55  up 1 day,  3:01, 2 users, load averages: 2.41 3.72 3.57

--- npc/csrc/mem/mrom-data/char-test.c
+++ npc/csrc/mem/mrom-data/char-test.c
@@ -1,9 +1,16 @@
+#include <stdint.h>
 #define UART_BASE 0x10000000L
 #define UART_TX 0x0L
 void _start()
 {
     *(volatile char *)(UART_BASE + UART_TX) = 'A';
     *(volatile char *)(UART_BASE + UART_TX) = '\n';
+    asm volatile(
+        "mv x1, %0\n"
+        // "ld x2, 0(x1)\n"
+        :
+        : "r"(0x20000000)
+        :);
     asm volatile("ebreak");
     while (1)
         ;
