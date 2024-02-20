70c528e7baaa647357afcfdef162949f5c71a103
tracer-ysyx <tracer@ysyx.org>
2024-02-20 16:39:44 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 16:39  up 10 days, 18:45, 4 users, load averages: 3.12 2.72 2.76

--- npc/include/common.h
+++ npc/include/common.h
@@ -69,19 +69,9 @@ typedef word_t vaddr_t;
   _Log(FMT_RED("[npc %s:%d %s] ") format "\n", \
        __FILENAME__, __LINE__, __func__, ##__VA_ARGS__)
 
-#define Assert(cond, format, ...)                                                                   \
-  do                                                                                                \
-  {                                                                                                 \
-    if (!(cond))                                                                                    \
-    {                                                                                               \
-      MUXDEF(CONFIG_TARGET_AM, printf(ANSI_FMT(format, ANSI_FG_RED) "\n", ##__VA_ARGS__),           \
-             (fflush(stdout), fprintf(stderr, ANSI_FMT(format, ANSI_FG_RED) "\n", ##__VA_ARGS__))); \
-      IFNDEF(CONFIG_TARGET_AM, extern FILE *log_fp; fflush(log_fp));                                \
-      extern void assert_fail_msg();                                                                \
-      assert_fail_msg();                                                                            \
-      assert(cond);                                                                                 \
-    }                                                                                               \
-  } while (0)
+#define Assert(cond, format, ...) \
+  Error(format, ##__VA_ARGS__);   \
+  assert(cond)
 
 enum
 {
