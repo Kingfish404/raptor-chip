f95363e57385ae0fae7f800221762a721cece9c8
tracer-ysyx <tracer@ysyx.org>
2024-02-26 14:50:47 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 14:50  up 16 days, 16:56, 1 user, load averages: 5.89 4.50 4.05

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -1,11 +1,17 @@
 #include <am.h>
 #include <ysyxsoc.h>
 #include <klib-macros.h>
+#include <string.h>
 
 extern char _heap_start;
 int main(const char *args);
 
 extern char _pmem_start;
+
+extern char data_start[];
+extern char data_size[];
+extern char data_load_start[];
+
 #define PMEM_SIZE (128 * 1024 * 1024)
 #define PMEM_END ((uintptr_t) & _pmem_start + PMEM_SIZE)
 
@@ -14,6 +20,14 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 #define MAINARGS ""
 #endif
 static const char mainargs[] = MAINARGS;
+
+void copy_data(void)
+{
+  if (data_start != data_load_start)
+  {
+    memcpy(data_start, data_load_start, (size_t)data_size);
+  }
+}
 
 void putch(char ch)
 {
@@ -29,6 +43,7 @@ void halt(int code)
 
 void _trm_init()
 {
+  copy_data();
   int ret = main(mainargs);
   halt(ret);
 }
