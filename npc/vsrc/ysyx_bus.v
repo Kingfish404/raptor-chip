94010f381e225f9eab9627ee57955ffe6dbe6613
tracer-ysyx <tracer@ysyx.org>
2024-02-26 21:34:08 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 21:34  up 16 days, 23:39, 5 users, load averages: 1.68 2.50 3.48

--- npc/vsrc/ysyx_bus.v
+++ npc/vsrc/ysyx_bus.v
@@ -139,11 +139,12 @@ module ysyx_BUS_ARBITER(
   wire [DATA_W-1:0] io_rdata = (io_master_araddr[2:2] == 1) ?
        io_master_rdata[63:32]:
        io_master_rdata[31:00];
+  wire [1:0] araddr_lo = io_master_araddr[1:0];
   assign rdata_o = (
-           ({DATA_W{io_master_araddr[1:0] == 2'b00}} & io_rdata) |
-           ({DATA_W{io_master_araddr[1:0] == 2'b01}} & {{8'0}, {io_rdata[DATA_W-1:8]}}) |
-           ({DATA_W{io_master_araddr[1:0] == 2'b10}} & {{16'0}, {io_rdata[DATA_W-1:16]}}) |
-           ({DATA_W{io_master_araddr[1:0] == 2'b11}} & {{24'0}, {io_rdata[DATA_W-1:24]}}) |
+           ({DATA_W{araddr_lo == 2'b00}} & io_rdata) |
+           ({DATA_W{araddr_lo == 2'b01}} & {{8'0}, {io_rdata[DATA_W-1:8]}}) |
+           ({DATA_W{araddr_lo == 2'b10}} & {{16'0}, {io_rdata[DATA_W-1:16]}}) |
+           ({DATA_W{araddr_lo == 2'b11}} & {{24'0}, {io_rdata[DATA_W-1:24]}}) |
          );
   // assign rdata_o = (io_master_araddr[2:2] == 1) ?
   //        io_master_rdata[63:32]:
