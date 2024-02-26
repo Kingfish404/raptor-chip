55c9a10108b7ad50ca1200986cc4f777b9448f98
tracer-ysyx <tracer@ysyx.org>
2024-02-26 13:03:56 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 13:03  up 16 days, 15:09, 2 users, load averages: 3.62 3.15 3.01

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -3,12 +3,12 @@ AM_SRCS := riscv/ysyxsoc/start.S \
            riscv/ysyxsoc/trap.S
 
 CFLAGS    += -fdata-sections -ffunction-sections
-LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
-						 --print-map \
+LDFLAGS   += -T $(AM_HOME)/scripts/linker.ysyxsoc.ld \
 						 --defsym=_pmem_start=0x20000000 \
 						 --defsym=_entry_offset=0x0 \
 						 --defsym=_stack_pointer=0x0f002000 \
-						 --defsym=_heap_start=0x0f000000
+						 --defsym=_heap_start=0x0f000000 \
+						 --print-map
 LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/riscv/ysyxsoc/include
