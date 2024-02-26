1434c76d956a3e9471aaf029d717d168fccf1482
tracer-ysyx <tracer@ysyx.org>
2024-02-26 21:34:21 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 21:34  up 16 days, 23:39, 5 users, load averages: 1.66 2.46 3.45

--- npc/vsrc/ysyx_bus.v
+++ npc/vsrc/ysyx_bus.v
@@ -142,9 +142,7 @@ module ysyx_BUS_ARBITER(
   wire [1:0] araddr_lo = io_master_araddr[1:0];
   assign rdata_o = (
            ({DATA_W{araddr_lo == 2'b00}} & io_rdata) |
-           ({DATA_W{araddr_lo == 2'b01}} & {{8'0}, {io_rdata[DATA_W-1:8]}}) |
-           ({DATA_W{araddr_lo == 2'b10}} & {{16'0}, {io_rdata[DATA_W-1:16]}}) |
-           ({DATA_W{araddr_lo == 2'b11}} & {{24'0}, {io_rdata[DATA_W-1:24]}}) |
+           ({DATA_W{araddr_lo == 2'b01}} & {{8'0}, {io_rdata[DATA_W-1:8]}})
          );
   // assign rdata_o = (io_master_araddr[2:2] == 1) ?
   //        io_master_rdata[63:32]:
