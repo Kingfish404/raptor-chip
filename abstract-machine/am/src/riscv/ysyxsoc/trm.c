efa7c972fb69128325190d0f8222d0a59783a819
tracer-ysyx <tracer@ysyx.org>
2024-03-03 23:55:19 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 23:55  up 23 days, 2 hrs, 1 user, load averages: 2.38 2.51 2.74

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -59,6 +59,10 @@ void init_uart(void)
   // outb(COM1 + 1, 0x01); // Enable receive interrupts.
 
   outb(UART16550_LCR, 0x3);
+  outb(UART16550_LCR, 0x3);
+  outb(UART16550_LCR, 0x3);
+  outb(UART16550_LCR, 0x3);
+  outb(UART16550_LCR, 0x3);
   volatile uint8_t lcr = inb(UART16550_LCR);
   // outb(UART16550_DL2, 0x0);
   // outb(UART16550_DL1, 0x1);
