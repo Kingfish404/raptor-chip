eff2dec77cb41f8231b09d77003e8c91a35e5068
tracer-ysyx <tracer@ysyx.org>
2024-03-03 22:49:42 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 22:49  up 23 days, 55 mins, 1 user, load averages: 3.56 5.43 6.65

--- npc/csrc/mem/mrom-data/char-test.c
+++ npc/csrc/mem/mrom-data/char-test.c
@@ -1,35 +1,7 @@
 #define UART_BASE 0x10000000L
 #define UART_TX 0x0L
-#include <stdint.h>
-
-static inline uint8_t inb(uintptr_t addr) { return *(volatile uint8_t *)addr; }
-static inline uint16_t inw(uintptr_t addr) { return *(volatile uint16_t *)addr; }
-static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; }
-
-static inline void outb(uintptr_t addr, uint8_t data) { *(volatile uint8_t *)addr = data; }
-static inline void outw(uintptr_t addr, uint16_t data) { *(volatile uint16_t *)addr = data; }
-static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
-
-#define COM1 0x10000000
-
 void _start()
 {
-
-    // Turn off the FIFO
-    outb(COM1 + 2, 0);
-
-    // 9600 baud, 8 data bits, 1 stop bit, parity off.
-    outb(COM1 + 3, 0x80); // Unlock divisor
-    outb(COM1 + 0, 115200 / 9600);
-    outb(COM1 + 1, 0);
-    outb(COM1 + 3, 0x03); // Lock divisor, 8 data bits.
-    outb(COM1 + 4, 0);
-    outb(COM1 + 1, 0x01); // Enable receive interrupts.
-
-    *(volatile char *)(UART_BASE + UART_TX) = 'A';
-    *(volatile char *)(UART_BASE + UART_TX) = 'A';
-    *(volatile char *)(UART_BASE + UART_TX) = 'A';
-    *(volatile char *)(UART_BASE + UART_TX) = 'A';
     *(volatile char *)(UART_BASE + UART_TX) = 'A';
     *(volatile char *)(UART_BASE + UART_TX) = '\n';
     asm volatile("ebreak");
