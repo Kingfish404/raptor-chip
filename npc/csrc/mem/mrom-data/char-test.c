4eda62274ada37593db197496819dfb2f1072fa2
tracer-ysyx <tracer@ysyx.org>
2024-03-03 22:45:14 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 22:45  up 23 days, 50 mins, 1 user, load averages: 3.47 7.86 7.70

--- npc/csrc/mem/mrom-data/char-test.c
+++ npc/csrc/mem/mrom-data/char-test.c
@@ -1,7 +1,25 @@
 #define UART_BASE 0x10000000L
 #define UART_TX 0x0L
+#include <stdint.h>
+
+static inline uint8_t inb(uintptr_t addr) { return *(volatile uint8_t *)addr; }
+static inline uint16_t inw(uintptr_t addr) { return *(volatile uint16_t *)addr; }
+static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; }
+
+static inline void outb(uintptr_t addr, uint8_t data) { *(volatile uint8_t *)addr = data; }
+static inline void outw(uintptr_t addr, uint16_t data) { *(volatile uint16_t *)addr = data; }
+static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
+
+#define COM1 0x10000000
+
 void _start()
 {
+    outb(COM1 + 3, 0x80); // Unlock divisor
+    outb(COM1 + 0, 115200 / 9600);
+    outb(COM1 + 1, 0);
+    outb(COM1 + 3, 0x03); // Lock divisor, 8 data bits.
+    outb(0x0f001fec, 0x0);
+
     *(volatile char *)(UART_BASE + UART_TX) = 'A';
     *(volatile char *)(UART_BASE + UART_TX) = '\n';
     asm volatile("ebreak");
