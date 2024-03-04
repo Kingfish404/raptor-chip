4734349a86c605f762ba19c2c1bb657403c0fbfe
tracer-ysyx <tracer@ysyx.org>
2024-03-04 13:02:51 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 13:02  up 23 days, 15:08, 1 user, load averages: 5.43 5.05 7.56

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -50,7 +50,8 @@ void init_uart(void)
 
   outb(COM1 + 3, 0x80); // Unlock divisor
   outb(COM1 + 1, 0);
-  outb(COM1 + 0, 115200 / 9600);
+  // outb(COM1 + 0, 115200 / 9600);
+  outb(COM1 + 0, 1);
   outb(COM1 + 3, 0x03); // Lock divisor, 8 data bits.
   // outb(0x0f001fec, 0x0);
   // asm volatile("ebreak");
