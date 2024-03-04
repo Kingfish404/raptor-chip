5d40e3b104d7b33038e2354702ca70f44106ccac
tracer-ysyx <tracer@ysyx.org>
2024-03-04 13:59:27 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 13:59  up 23 days, 16:05, 1 user, load averages: 2.84 3.33 3.59

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -55,7 +55,7 @@ void init_uart(void)
   outb(UART16550_LCR, 0x03);
   inb(UART16550_LSR);
   uint8_t ch = inb(UART16550_LSR);
-  for (size_t i = 0; i < 10; i++)
+  for (size_t i = 0; i < 100; i++)
   {
     asm volatile("nop");
   }
