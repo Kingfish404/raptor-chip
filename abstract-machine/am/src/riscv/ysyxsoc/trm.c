a9dae2650d54910e576b8ae03181aac404891a1d
tracer-ysyx <tracer@ysyx.org>
2024-03-05 15:58:09 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64 15:58  up 24 days, 18:03, 5 users, load averages: 6.62 5.41 4.83

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -23,8 +23,8 @@ static const char mainargs[] = MAINARGS;
 
 void putch(char ch)
 {
-  while ((inb(UART16550_LSR) & (0x1 << 5)) == 0x0)
-    ;
+  // while ((inb(UART16550_LSR) & (0x1 << 5)) == 0x0)
+  //   ;
   outb(UART16550_TX, ch);
 }
 
