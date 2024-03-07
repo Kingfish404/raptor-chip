198b3633c245aedf0030922be0bf9a3127667d53
tracer-ysyx <tracer@ysyx.org>
2024-03-08 01:19:55 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.3.0 Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000 arm64  1:19  up 27 days,  3:25, 5 users, load averages: 4.57 4.45 4.55

--- npc/vsrc/ysyx_bus.v
+++ npc/vsrc/ysyx_bus.v
@@ -215,6 +215,7 @@ module ysyx_BUS_ARBITER(
           if (
             (io_master_awaddr >= 'h10000000 && io_master_awaddr <= 'h10000005) ||
             (io_master_araddr >= 'h10000001 && io_master_araddr <= 'h10000005) ||
+            (io_master_araddr >= 'h10001000 && io_master_araddr <= 'h10001fff) ||
             (io_master_awaddr >= 'h30000000 && io_master_awaddr <= 'h40000000) ||
             (0)
           )
