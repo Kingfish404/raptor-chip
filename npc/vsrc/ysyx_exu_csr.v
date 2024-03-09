a8af5b9908ddb6174b1c8aeccd0931d7c5db90cb
tracer-ysyx <tracer@ysyx.org>
2024-03-09 22:45:43 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Wed Feb 21 21:44:43 PST 2024; root:xnu-10063.101.15~2/RELEASE_ARM64_T6000 arm64 22:45  up 19:08, 1 user, load averages: 1.89 2.13 2.48

--- npc/vsrc/ysyx_exu_csr.v
+++ npc/vsrc/ysyx_exu_csr.v
@@ -16,6 +16,8 @@ module ysyx_CSR_Reg(
   parameter MEPC_IDX    = 'h2;
   parameter MTVEC_IDX   = 'h3;
   parameter MSTATUS_IDX = 'h4;
+  parameter MVENDORID_IDX = 'h5;
+  parameter MARCHID_IDX = 'h6;
 
   parameter R_W = 12;
   parameter BIT_W = `ysyx_W_WIDTH;
@@ -29,14 +31,21 @@ module ysyx_CSR_Reg(
     ({3{waddr==`ysyx_CSR_MCAUSE}}) & (MCAUSE_IDX) |
     ({3{waddr==`ysyx_CSR_MEPC}})   & (MEPC_IDX)   |
     ({3{waddr==`ysyx_CSR_MTVEC}})  & (MTVEC_IDX)  |
-    ({3{waddr==`ysyx_CSR_MSTATUS}})& (MSTATUS_IDX)
+    ({3{waddr==`ysyx_CSR_MSTATUS}})& (MSTATUS_IDX) |
+    ({3{waddr==`ysyx_CSR_MVENDORID}})& (MVENDORID_IDX) |
+    ({3{waddr==`ysyx_CSR_MARCHID}})& (MARCHID_IDX)
   );
   assign csr_addr_add1 = (
     ({3{waddr_add1==`ysyx_CSR_MCAUSE}}) & (MCAUSE_IDX) |
     ({3{waddr_add1==`ysyx_CSR_MEPC}})   & (MEPC_IDX)   |
     ({3{waddr_add1==`ysyx_CSR_MTVEC}})  & (MTVEC_IDX)  |
-    ({3{waddr_add1==`ysyx_CSR_MSTATUS}})& (MSTATUS_IDX)
+    ({3{waddr_add1==`ysyx_CSR_MSTATUS}})& (MSTATUS_IDX) |
+    ({3{waddr_add1==`ysyx_CSR_MVENDORID}})& (MVENDORID_IDX) |
+    ({3{waddr_add1==`ysyx_CSR_MARCHID}})& (MARCHID_IDX)
   );
+
+  assign csr[MVENDORID_IDX] = 32'h79737978;
+  assign csr[MARCHID_IDX]   = 32'h150be98;
 
   reg [BIT_W-1:0] csr[0:7];
   always @(posedge clk) begin
