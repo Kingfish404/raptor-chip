0382963f45a7217052493ecc78891546bdb3acf8
tracer-ysyx <tracer@ysyx.org>
2024-03-09 22:55:13 +0800

    > compile NEMU ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Wed Feb 21 21:44:43 PST 2024; root:xnu-10063.101.15~2/RELEASE_ARM64_T6000 arm64 22:55  up 19:17, 1 user, load averages: 2.04 2.70 2.87

--- nemu/src/isa/riscv32/local-include/reg.h
+++ nemu/src/isa/riscv32/local-include/reg.h
@@ -24,6 +24,7 @@ static inline int check_reg_idx(int idx) {
 }
 
 static inline int check_sr_idx(int idx) {
+  printf("idx: %d\n", idx);
   IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < 4096));
   return idx;
 }
