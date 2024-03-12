26b0a1eef7ad7ee838bb07643be883db21bf6f93
tracer-ysyx <tracer@ysyx.org>
2024-03-12 14:01:57 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Wed Feb 21 21:44:43 PST 2024; root:xnu-10063.101.15~2/RELEASE_ARM64_T6000 arm64 14:01  up 3 days, 42 mins, 2 users, load averages: 5.67 4.58 4.32

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -4,7 +4,7 @@ AM_SRCS := riscv/ysyxsoc/start.S \
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ysyxsoc.ld \
-						 --defsym=_pmem_start=0x20000000 \
+						 --defsym=_pmem_start=0x30000000 \
 						 --defsym=_entry_offset=0x0 \
 						 --defsym=_stack_pointer=0x0f002000 \
 						 --defsym=_heap_start=0x0f000000 \
