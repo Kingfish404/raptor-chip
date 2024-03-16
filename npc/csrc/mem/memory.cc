8f5709803f6cee35d04449717c2431347aaf5fd0
tracer-ysyx <tracer@ysyx.org>
2024-03-16 17:57:28 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Wed Feb 21 21:44:43 PST 2024; root:xnu-10063.101.15~2/RELEASE_ARM64_T6000 arm64 17:57  up 6 days, 17:04, 5 users, load averages: 6.33 6.16 5.62

--- npc/csrc/mem/memory.cc
+++ npc/csrc/mem/memory.cc
@@ -110,8 +110,8 @@ extern "C" void pmem_read(word_t raddr, word_t *data)
 
 extern "C" void pmem_write(word_t waddr, word_t wdata, char wmask)
 {
-    Log("waddr: 0x%x, wdata: 0x%x, wmask = 0x%x",
-           waddr, wdata, wmask);
+    // Log("waddr: 0x%x, wdata: 0x%x, wmask = 0x%x",
+    //        waddr, wdata, wmask);
 #ifdef CONFIG_SOFT_MMIO
     // SERIAL_MMIO: hex "MMIO address of the serial controller"
     if (waddr == SERIAL_PORT)
