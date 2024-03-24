f2fc6eb67fec94917d27fb7695f8acd0ab97fa94
tracer-ysyx <tracer@ysyx.org>
2024-03-24 18:22:33 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Wed Feb 21 21:44:43 PST 2024; root:xnu-10063.101.15~2/RELEASE_ARM64_T6000 arm64 18:22  up 14 days,  3:58, 2 users, load averages: 5.44 4.52 4.29

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -98,7 +98,7 @@ void _trm_init()
 {
   init_uart();
   volatile uint32_t *sdram = (uint32_t *)0xa1fffffc;
-  volatile uint8_t *sdram_u8 = (uint8_t *)0xa1fffff1;
+  volatile uint8_t *sdram_u8 = (uint8_t *)0xa1fffff0;
   putch('\n');
   // *sdram = 0x12345678;
   // for (int i = 0; i < 0xf; i++)
