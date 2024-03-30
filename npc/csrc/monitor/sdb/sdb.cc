5d7a4fb5f9b0b7357d5a1655aeaacd42b1c11c37
tracer-ysyx <tracer@ysyx.org>
2024-03-31 00:12:22 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64  0:12  up 1 day, 12:53, 2 users, load averages: 2.82 2.05 2.25

--- npc/csrc/monitor/sdb/sdb.cc
+++ npc/csrc/monitor/sdb/sdb.cc
@@ -267,9 +267,9 @@ void sdb_sim_init(int argc, char **argv)
   contextp->timeInc(1);
 
 #ifdef CONFIG_NVBoard
-  nvboard_bind_pin(
-      &CONCAT(VERILOG_PREFIX, __DOT__gpio_out),
-      16, LD15, LD14, LD13, LD12, LD11, LD10, LD9, LD8, LD7, LD6, LD5, LD4, LD3, LD2, LD1, LD0);
+  // nvboard_bind_pin(
+  //     &CONCAT(VERILOG_PREFIX, __DOT__gpio_out),
+  //     16, LD15, LD14, LD13, LD12, LD11, LD10, LD9, LD8, LD7, LD6, LD5, LD4, LD3, LD2, LD1, LD0);
 
   nvboard_init();
 #endif
