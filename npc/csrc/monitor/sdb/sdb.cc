ee793031efa66a42ed8fdb6e68c2105064b345d6
tracer-ysyx <tracer@ysyx.org>
2024-03-31 21:03:09 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64 21:03  up 2 days,  9:03, 4 users, load averages: 3.88 6.22 5.29

--- npc/csrc/monitor/sdb/sdb.cc
+++ npc/csrc/monitor/sdb/sdb.cc
@@ -290,7 +290,7 @@ void sdb_sim_init(int argc, char **argv)
       1, UART_TX);
   nvboard_bind_pin(
 
-      &top->rootp->ysyxSoCFull__DOT__asic__DOT__luart__DOT__muart__DOT__Uregs__DOT__transmitter__DOT__tf_pop,
+      &top->rootp->ysyxSoCFull__DOT__asic__DOT__luart__DOT__muart__DOT__Uregs__DOT__i_uart_sync_flops__DOT__flop_0,
       1, UART_RX);
 
   nvboard_init();
