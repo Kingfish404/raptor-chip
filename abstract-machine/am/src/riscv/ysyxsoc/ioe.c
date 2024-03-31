4f234e64ad9b873f3352c84a96df3ca6df06ffa3
tracer-ysyx <tracer@ysyx.org>
2024-03-31 20:18:52 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64 20:18  up 2 days,  8:19, 4 users, load averages: 2.96 2.50 2.78

--- abstract-machine/am/src/riscv/ysyxsoc/ioe.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe.c
@@ -1,4 +1,5 @@
 #include <am.h>
+#include <ysyxsoc.h>
 #include <klib-macros.h>
 
 void __am_timer_init();
@@ -6,6 +7,18 @@ void __am_timer_init();
 void __am_timer_rtc(AM_TIMER_RTC_T *);
 void __am_timer_uptime(AM_TIMER_UPTIME_T *);
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *);
+
+static void __am_uart_tx(AM_UART_TX_T *send)
+{
+    while ((inb(UART16550_LSR) & (0x1 << 5)) == 0x0)
+        ;
+    outb(UART16550_TX, send->data);
+}
+
+static void __am_uart_rx(AM_UART_RX_T *recv)
+{
+    recv->data = inb(UART16550_RX);
+}
 
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg)
 {
@@ -16,11 +29,12 @@ static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true; }
 
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
+    [AM_UART_TX] = NULL,
+    [AM_UART_RX] = NULL,
     [AM_TIMER_CONFIG] = __am_timer_config,
     [AM_TIMER_RTC] = __am_timer_rtc,
     [AM_TIMER_UPTIME] = __am_timer_uptime,
-    // [AM_INPUT_CONFIG] = __am_input_config,
-    // [AM_INPUT_KEYBRD] = __am_input_keybrd,
+    [AM_INPUT_KEYBRD] = __am_input_keybrd,
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }
