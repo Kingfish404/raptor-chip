1b3f33bc49cf9289c059e06b3e7be8b66a17ecc1
tracer-ysyx <tracer@ysyx.org>
2024-04-11 01:57:26 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64  1:57  up 11 days, 16:03, 2 users, load averages: 4.66 4.57 4.37

--- abstract-machine/am/src/riscv/ysyxsoc/input.c
+++ abstract-machine/am/src/riscv/ysyxsoc/input.c
@@ -1,7 +1,14 @@
 #include <am.h>
+#include <ysyxsoc.h>
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd)
 {
   kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  int keycode = inl(KBD_ADDR);
+  if (keycode != 0)
+  {
+    kbd->keydown = 1;
+    keycode = keycode;
+  }
+  kbd->keycode = keycode;
 }
