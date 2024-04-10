b1daf4391156502e4a4148c13c86731a3f1c74d9
tracer-ysyx <tracer@ysyx.org>
2024-04-11 02:28:22 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64  2:28  up 11 days, 16:34, 2 users, load averages: 4.47 3.65 3.42

--- abstract-machine/am/src/riscv/ysyxsoc/ioe.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe.c
@@ -25,7 +25,7 @@ static void __am_timer_config(AM_TIMER_CONFIG_T *cfg)
     cfg->present = true;
     cfg->has_rtc = true;
 }
-static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = false; }
+static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true; }
 
 static void __am_uart_config(AM_UART_CONFIG_T *cfg) { cfg->present = true; }
 
