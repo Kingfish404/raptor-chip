33488ecd763654089b1c50ad4c7fa82a6877e890
tracer-ysyx <tracer@ysyx.org>
2024-04-12 01:02:24 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64  1:02  up 12 days, 13:45, 9 users, load averages: 3.19 3.71 3.84

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -61,8 +61,12 @@ void halt(int code)
     ;
 }
 
+size_t fsb_start_time, fsb_end_time;
+size_t ssb_start_time, ssb_end_time;
+
 __attribute__((section(".first_boot"))) void _first_stage_bootloader(void)
 {
+  fsb_start_time = ((*((uint32_t *)RTC_ADDR + 4)) << 32) + *((uint32_t *)RTC_ADDR);
   if ((size_t)_second_boot_start != (size_t)_second_boot_load_start)
   {
     size_t text_size = _second_boot_end - _second_boot_start;
@@ -71,32 +75,26 @@ __attribute__((section(".first_boot"))) void _first_stage_bootloader(void)
       _second_boot_start[i] = _second_boot_load_start[i];
     }
   }
+  fsb_end_time = ((*((uint32_t *)RTC_ADDR + 4)) << 32) + *((uint32_t *)RTC_ADDR);
   _second_stage_bootloader();
 }
 
-size_t boot_text_time_s, boot_text_time_e;
-
 __attribute__((section(".second_boot"))) void _second_stage_bootloader()
 {
-  boot_text_time_s = ((*((uint32_t *)RTC_ADDR + 4)) << 32) + *((uint32_t *)RTC_ADDR);
+  ssb_start_time = ((*((uint32_t *)RTC_ADDR + 4)) << 32) + *((uint32_t *)RTC_ADDR);
   if ((size_t)_text_start != (size_t)_text_load_start)
   {
     size_t text_size = _text_end - _text_start;
-    // size_t text_size_u64_fix = text_size / 8;
-    // for (size_t i = 0; i < text_size_u64_fix; i++)
-    // {
-    //   ((uint64_t *)_text_start)[i] = ((uint64_t *)_text_load_start)[i];
-    // }
-    // for (size_t i = text_size_u64_fix * 8; i < text_size; i++)
-    // {
-    //   _text_start[i] = _text_load_start[i];
-    // }
-    for (size_t i = 0; i < text_size; i++)
+    size_t text_size_u64_fix = text_size / 8;
+    for (size_t i = 0; i < text_size_u64_fix; i++)
+    {
+      ((uint64_t *)_text_start)[i] = ((uint64_t *)_text_load_start)[i];
+    }
+    for (size_t i = text_size_u64_fix * 8; i < text_size; i++)
     {
       _text_start[i] = _text_load_start[i];
     }
   }
-  boot_text_time_e = ((*((uint32_t *)RTC_ADDR + 4)) << 32) + *((uint32_t *)RTC_ADDR);
   if ((size_t)_rodata_start != (size_t)_rodata_load_start)
   {
     size_t rodata_size = _rodata_end - _rodata_start;
@@ -107,6 +105,7 @@ __attribute__((section(".second_boot"))) void _second_stage_bootloader()
     size_t data_size = _data_end - _data_start;
     memcpy(_data_start, _data_load_start, (size_t)data_size);
   }
+  ssb_end_time = ((*((uint32_t *)RTC_ADDR + 4)) << 32) + *((uint32_t *)RTC_ADDR);
   _trm_init();
 }
 
@@ -114,7 +113,7 @@ void _trm_init()
 {
   init_uart();
   ioe_init();
-  printf("copy text section time: %ld us\n", boot_text_time_e - boot_text_time_s);
+  printf("second boot time: %ld us\n", ssb_end_time - ssb_start_time);
   asm volatile("ebreak");
   uint32_t mvendorid, marchid;
   asm volatile(
