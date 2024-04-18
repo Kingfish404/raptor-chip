f183ca67b0b5d53a5585180efc64bfd0f79a7f15
tracer-ysyx <tracer@ysyx.org>
2024-04-18 12:01:40 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64 12:01  up 17 days, 16:08, 11 users, load averages: 2.75 3.15 2.83

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -64,30 +64,30 @@ void halt(int code)
 __attribute__((section(".first_boot"))) void _first_stage_bootloader(void)
 {
   volatile uint8_t data;
-  uint8_t *p = 0xc0000000;       // CHIPLINK_MEM_BASE
-  uint8_t *p_sdram = 0xa0000000; // SDM_BASE
-  for (int i = 0; i < 10; i++)
-  {
-    asm volatile(
-        "sb	a5,3(sp)\n"
-        "lbu t0, 0(%1)\n"
-        : "=r"(data)
-        : "r"(&p[i]), "r"(&p_sdram[i])
-        :);
-  }
+  // uint8_t *p = 0xc0000000;       // CHIPLINK_MEM_BASE
+  // uint8_t *p_sdram = 0xa0000000; // SDM_BASE
+  // for (int i = 0; i < 10; i++)
+  // {
+  //   asm volatile(
+  //       "sb	a5,3(sp)\n"
+  //       "lbu t0, 0(%1)\n"
+  //       : "=r"(data)
+  //       : "r"(&p[i]), "r"(&p_sdram[i])
+  //       :);
+  // }
   // asm volatile("ebreak");
   // return;
 
   if ((size_t)_second_boot_start != (size_t)_second_boot_load_start)
   {
     size_t text_size = _second_boot_end - _second_boot_start;
-    // size_t text_size_u64_fix = text_size / 8;
-    // for (size_t i = 0; i < text_size_u64_fix; i++)
-    // {
-    //   ((uint64_t *)_second_boot_start)[i] = ((uint64_t *)_second_boot_load_start)[i];
-    // }
-    // for (size_t i = text_size_u64_fix * 8; i < text_size; i++)
-    for (size_t i = 0; i < text_size; i++)
+    size_t text_size_u64_fix = text_size / 8;
+    for (size_t i = 0; i < text_size_u64_fix; i++)
+    {
+      ((uint64_t *)_second_boot_start)[i] = ((uint64_t *)_second_boot_load_start)[i];
+    }
+    for (size_t i = text_size_u64_fix * 8; i < text_size; i++)
+    // for (size_t i = 0; i < text_size; i++)
     {
       _second_boot_start[i] = _second_boot_load_start[i];
     }
@@ -103,13 +103,13 @@ __attribute__((section(".second_boot"))) void _second_stage_bootloader()
   if ((size_t)_text_start != (size_t)_text_load_start)
   {
     size_t text_size = _text_end - _text_start;
-    // size_t text_size_u64_fix = text_size / 8;
-    // for (size_t i = 0; i < text_size_u64_fix; i++)
-    // {
-    //   ((uint64_t *)_text_start)[i] = ((uint64_t *)_text_load_start)[i];
-    // }
-    // for (size_t i = text_size_u64_fix * 8; i < text_size; i++)
-    for (size_t i = 0; i < text_size; i++)
+    size_t text_size_u64_fix = text_size / 8;
+    for (size_t i = 0; i < text_size_u64_fix; i++)
+    {
+      ((uint64_t *)_text_start)[i] = ((uint64_t *)_text_load_start)[i];
+    }
+    for (size_t i = text_size_u64_fix * 8; i < text_size; i++)
+    // for (size_t i = 0; i < text_size; i++)
     {
       _text_start[i] = _text_load_start[i];
     }
