1c81763b8a1879ed47f0fddc5160c8e2346f7fe2
tracer-ysyx <tracer@ysyx.org>
2024-04-18 12:05:55 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64 12:05  up 17 days, 16:12, 11 users, load averages: 2.70 2.93 2.81

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -81,13 +81,13 @@ __attribute__((section(".first_boot"))) void _first_stage_bootloader(void)
   if ((size_t)_second_boot_start != (size_t)_second_boot_load_start)
   {
     size_t text_size = _second_boot_end - _second_boot_start;
-    // size_t text_size_u64_fix = text_size / 8;
-    // for (size_t i = 0; i < text_size_u64_fix; i++)
-    // {
-    //   ((uint64_t *)_second_boot_start)[i] = ((uint64_t *)_second_boot_load_start)[i];
-    // }
-    // for (size_t i = text_size_u64_fix * 8; i < text_size; i++)
-    for (size_t i = 0; i < text_size; i++)
+    size_t text_size_u64_fix = text_size / 8;
+    for (size_t i = 0; i < text_size_u64_fix; i++)
+    {
+      ((uint64_t *)_second_boot_start)[i] = ((uint64_t *)_second_boot_load_start)[i];
+    }
+    for (size_t i = text_size_u64_fix * 8; i < text_size; i++)
+    // for (size_t i = 0; i < text_size; i++)
     {
       _second_boot_start[i] = _second_boot_load_start[i];
     }
@@ -131,8 +131,8 @@ __attribute__((section(".second_boot"))) void _second_stage_bootloader()
 void _trm_init()
 {
   init_uart();
-  printf("[%d\t] first stage boot loader finish\n", ssb_start_time);
-  printf("[%d\t] second stage boot loader finish\n", ssb_end_time);
+  printf("[%d] first stage boot loader finish\n", ssb_start_time);
+  printf("[%d] second stage boot loader finish\n", ssb_end_time);
   ioe_init();
   uint32_t mvendorid, marchid;
   asm volatile(
