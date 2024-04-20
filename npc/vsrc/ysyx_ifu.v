0b1e68b4df60a51969f13265f5025c4b826ccbb7
tracer-ysyx <tracer@ysyx.org>
2024-04-20 17:36:51 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64 17:36  up 18 days, 20:40, 2 users, load averages: 4.79 5.44 5.18

--- npc/vsrc/ysyx_ifu.v
+++ npc/vsrc/ysyx_ifu.v
@@ -40,7 +40,7 @@ module ysyx_IFU (
   assign ifu_arvalid_o = arvalid & l1_cache_miss;
   // assign inst_o = ifu_rvalid ? ifu_rdata : inst_ifu;
   assign inst_o = ifu_rvalid ? ifu_rdata : l1_icache[addr_idx];
-  assign valid_o = ifu_rvalid | valid;
+  assign valid_o = ifu_rvalid | valid | !l1_cache_miss;
 
   `ysyx_BUS_FSM();
   always @(posedge clk)
