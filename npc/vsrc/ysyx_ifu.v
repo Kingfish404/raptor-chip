811ab5eea3232d5da95105b3ec78035d599c692e
tracer-ysyx <tracer@ysyx.org>
2024-04-20 17:39:09 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64 17:39  up 18 days, 20:42, 2 users, load averages: 5.54 5.51 5.24

--- npc/vsrc/ysyx_ifu.v
+++ npc/vsrc/ysyx_ifu.v
@@ -25,6 +25,7 @@ module ysyx_IFU (
   reg [32-1:0] l1_icache[256-1:0];
   reg [24-1:0] l1_icache_tag[256-1:0];
   reg [256-1:0] l1_icache_valid = 0;
+  reg l1_icache_hit = 0;
 
   wire arvalid;
   wire [24-1:0] addr_tag = ifu_araddr_o[ADDR_W-1:8];
@@ -40,7 +41,7 @@ module ysyx_IFU (
   assign ifu_arvalid_o = arvalid & l1_cache_miss;
   // assign inst_o = ifu_rvalid ? ifu_rdata : inst_ifu;
   assign inst_o = ifu_rvalid ? ifu_rdata : l1_icache[addr_idx];
-  assign valid_o = ifu_rvalid | valid | !l1_cache_miss;
+  assign valid_o = ifu_rvalid | valid;
 
   `ysyx_BUS_FSM();
   always @(posedge clk)
@@ -61,6 +62,14 @@ module ysyx_IFU (
               l1_icache_tag[addr_idx] <= addr_tag;
               l1_icache_valid[addr_idx] <= 1;
             end
+          if (l1_cache_miss)
+            begin
+              l1_icache_valid[addr_idx] <= 0;
+            end
+          else
+            begin
+              l1_icache_hit <= 1;
+            end
           if (state == `ysyx_IDLE)
             begin
               if (prev_valid)
@@ -78,6 +87,7 @@ module ysyx_IFU (
                 begin
                   pvalid <= 0;
                   valid <= 0;
+                  l1_icache_hit <= 0;
                 end
             end
         end
