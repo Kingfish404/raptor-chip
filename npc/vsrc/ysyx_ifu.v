ca2e8701b421d9898ffec3c1585df03c9281efd5
tracer-ysyx <tracer@ysyx.org>
2024-04-20 17:13:18 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64 17:13  up 18 days, 20:16, 2 users, load averages: 3.07 3.87 4.67

--- npc/vsrc/ysyx_ifu.v
+++ npc/vsrc/ysyx_ifu.v
@@ -1,45 +1,70 @@
 `include "ysyx_macro.v"
 
 module ysyx_IFU (
-  input clk, rst,
+    input clk, rst,
 
-  input prev_valid, next_ready,
-  output valid_o, ready_o,
+    input prev_valid, next_ready,
+    output valid_o, ready_o,
 
-  // for bus
-  output [DATA_W-1:0] ifu_araddr_o,
-  output ifu_arvalid_o,
-  input [DATA_W-1:0] ifu_rdata,
-  input ifu_rvalid,
+    // for bus
+    output [DATA_W-1:0] ifu_araddr_o,
+    output ifu_arvalid_o,
+    input [DATA_W-1:0] ifu_rdata,
+    input ifu_rvalid,
 
-  input [ADDR_W-1:0] pc, npc,
-  output [DATA_W-1:0] inst_o,
-  output reg [DATA_W-1:0] pc_o
-);
+    input [ADDR_W-1:0] pc, npc,
+    output [DATA_W-1:0] inst_o,
+    output reg [DATA_W-1:0] pc_o
+  );
   parameter ADDR_W = 32;
   parameter DATA_W = 32;
 
   reg [DATA_W-1:0] inst_ifu = 0;
   reg state, valid;
   reg pvalid;
+  reg [256-1:0] l1_icache[32-1:0];
+  reg [256-1:0] l1_icache_tag[24-1:0];
+
   wire arvalid;
+  wire [24-1:0] addr_tag = ifu_araddr_o[ADDR_W-1:9];
 
   `ysyx_BUS_FSM();
-  always @(posedge clk) begin
-    if (rst) begin
-      valid <= 0; pvalid <= 1; inst_ifu <= 0;
+  always @(posedge clk)
+    begin
+      if (rst)
+        begin
+          valid <= 0;
+          pvalid <= 1;
+          inst_ifu <= 0;
+        end
+      else
+        begin
+          pc_o <= pc;
+          if (ifu_rvalid)
+            begin
+              inst_ifu <= ifu_rdata;
+            end
+          if (state == `ysyx_IDLE)
+            begin
+              if (prev_valid)
+                begin
+                  pvalid <= prev_valid;
+                end
+              if (ifu_rvalid)
+                begin
+                  valid <= 1;
+                end
+            end
+          else if (state == `ysyx_WAIT_READY)
+            begin
+              if (next_ready == 1)
+                begin
+                  pvalid <= 0;
+                  valid <= 0;
+                end
+            end
+        end
     end
-    else begin
-      pc_o <= pc;
-      if (ifu_rvalid) begin inst_ifu <= ifu_rdata; end
-      if (state == `ysyx_IDLE) begin
-        if (prev_valid) begin pvalid <= prev_valid; end
-        if (ifu_rvalid) begin valid <= 1; end
-      end else if (state == `ysyx_WAIT_READY) begin
-        if (next_ready == 1) begin pvalid <= 0; valid <= 0; end
-      end
-    end
-  end
   assign ready_o = !valid_o;
 
   assign arvalid = pvalid;
