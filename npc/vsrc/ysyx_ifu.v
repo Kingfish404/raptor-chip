963d97dc43565388401ba079c05edd033887ec0d
tracer-ysyx <tracer@ysyx.org>
2024-04-20 17:24:41 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64 17:24  up 18 days, 20:27, 2 users, load averages: 4.64 4.38 4.53

--- npc/vsrc/ysyx_ifu.v
+++ npc/vsrc/ysyx_ifu.v
@@ -55,6 +55,8 @@ module ysyx_IFU (
           if (ifu_rvalid)
             begin
               inst_ifu <= ifu_rdata;
+              l1_icache[addr_idx] <= ifu_rdata;
+              l1_icache_tag[addr_idx] <= addr_tag;
             end
           if (state == `ysyx_IDLE)
             begin
