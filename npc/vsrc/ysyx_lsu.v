116f6b28de56001902602fe44920797065d69ad6
tracer-ysyx <tracer@ysyx.org>
2024-04-21 10:23:10 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64 10:23  up 19 days,  9:50, 2 users, load averages: 4.07 2.73 2.90

--- npc/vsrc/ysyx_lsu.v
+++ npc/vsrc/ysyx_lsu.v
@@ -1,35 +1,35 @@
 `include "ysyx_macro.v"
 
 module ysyx_LSU(
-  input clk,
-  input idu_valid,
-  // from exu
-  input [ADDR_W-1:0] addr,
-  input ren, wen, lsu_avalid,
-  input [3:0] alu_op,
-  input [DATA_W-1:0] wdata,
-  // to exu
-  output [DATA_W-1:0] rdata_o,
-  output reg rvalid_o,
-  output reg wready_o,
+    input clk,
+    input idu_valid,
+    // from exu
+    input [ADDR_W-1:0] addr,
+    input ren, wen, lsu_avalid,
+    input [3:0] alu_op,
+    input [DATA_W-1:0] wdata,
+    // to exu
+    output [DATA_W-1:0] rdata_o,
+    output reg rvalid_o,
+    output reg wready_o,
 
-  // to bus load
-  output [DATA_W-1:0] lsu_araddr_o,
-  output lsu_arvalid_o,
-  output [7:0] lsu_rstrb_o,
-  // from bus load
-  input [DATA_W-1:0] lsu_rdata,
-  input lsu_rvalid,
+    // to bus load
+    output [DATA_W-1:0] lsu_araddr_o,
+    output lsu_arvalid_o,
+    output [7:0] lsu_rstrb_o,
+    // from bus load
+    input [DATA_W-1:0] lsu_rdata,
+    input lsu_rvalid,
 
-  // to bus store
-  output [DATA_W-1:0] lsu_awaddr_o,
-  output lsu_awvalid_o,
-  output [DATA_W-1:0] lsu_wdata_o,
-  output [7:0] lsu_wstrb_o,
-  output lsu_wvalid_o,
-  // from bus store
-  input reg lsu_wready
-);
+    // to bus store
+    output [DATA_W-1:0] lsu_awaddr_o,
+    output lsu_awvalid_o,
+    output [DATA_W-1:0] lsu_wdata_o,
+    output [7:0] lsu_wstrb_o,
+    output lsu_wvalid_o,
+    // from bus store
+    input reg lsu_wready
+  );
   parameter ADDR_W = 32, DATA_W = 32;
 
   reg [ADDR_W-1:0] lsu_araddr;
@@ -62,32 +62,35 @@ module ysyx_LSU(
   wire [32-L1D_LEN-2-1:0] addr_tag = lsu_araddr_o[ADDR_W-1:L1D_LEN+2];
   wire [L1D_LEN-1:0] addr_idx = lsu_araddr_o[L1D_LEN+2-1:0+2];
   wire l1d_cache_hit = (
+         idu_valid &
          l1d_valid[addr_idx] == 1'b1) & (l1d_tag[addr_idx] == addr_tag);
 
   // load/store unit
   assign wstrb = (
-    ({8{alu_op == `ysyx_ALU_OP_SB}} & 8'h1) | 
-    ({8{alu_op == `ysyx_ALU_OP_SH}} & 8'h3) | 
-    ({8{alu_op == `ysyx_ALU_OP_SW}} & 8'hf)
-  );
+           ({8{alu_op == `ysyx_ALU_OP_SB}} & 8'h1) |
+           ({8{alu_op == `ysyx_ALU_OP_SH}} & 8'h3) |
+           ({8{alu_op == `ysyx_ALU_OP_SW}} & 8'hf)
+         );
   assign rstrb = (
-    ({8{alu_op == `ysyx_ALU_OP_LB}} & 8'h1) | 
-    ({8{alu_op == `ysyx_ALU_OP_LBU}} & 8'h1) | 
-    ({8{alu_op == `ysyx_ALU_OP_LH}} & 8'h3) | 
-    ({8{alu_op == `ysyx_ALU_OP_LHU}} & 8'h3) | 
-    ({8{alu_op == `ysyx_ALU_OP_LW}} & 8'hf)
-  );
+           ({8{alu_op == `ysyx_ALU_OP_LB}} & 8'h1) |
+           ({8{alu_op == `ysyx_ALU_OP_LBU}} & 8'h1) |
+           ({8{alu_op == `ysyx_ALU_OP_LH}} & 8'h3) |
+           ({8{alu_op == `ysyx_ALU_OP_LHU}} & 8'h3) |
+           ({8{alu_op == `ysyx_ALU_OP_LW}} & 8'hf)
+         );
   assign rdata_o = (
-    ({DATA_W{alu_op == `ysyx_ALU_OP_LB}} & (rdata[7] ? rdata | 'hffffff00 : rdata & 'hff)) | 
-    ({DATA_W{alu_op == `ysyx_ALU_OP_LBU}} & rdata & 'hff) | 
-    ({DATA_W{alu_op == `ysyx_ALU_OP_LH}} & (rdata[15] ? rdata | 'hffff0000 : rdata & 'hffff)) | 
-    ({DATA_W{alu_op == `ysyx_ALU_OP_LHU}} & rdata & 'hffff) | 
-    ({DATA_W{alu_op == `ysyx_ALU_OP_LW}} & rdata)
-  );
+           ({DATA_W{alu_op == `ysyx_ALU_OP_LB}} & (rdata[7] ? rdata | 'hffffff00 : rdata & 'hff)) |
+           ({DATA_W{alu_op == `ysyx_ALU_OP_LBU}} & rdata & 'hff) |
+           ({DATA_W{alu_op == `ysyx_ALU_OP_LH}} & (rdata[15] ? rdata | 'hffff0000 : rdata & 'hffff)) |
+           ({DATA_W{alu_op == `ysyx_ALU_OP_LHU}} & rdata & 'hffff) |
+           ({DATA_W{alu_op == `ysyx_ALU_OP_LW}} & rdata)
+         );
 
-  always @(posedge clk) begin
-    if (idu_valid) begin
-      lsu_araddr <= addr;
+  always @(posedge clk)
+    begin
+      if (idu_valid)
+        begin
+          lsu_araddr <= addr;
+        end
     end
-  end
 endmodule
