dce346a71c252e1fddeebeb5851571c5ab735d56
tracer-ysyx <tracer@ysyx.org>
2024-04-21 10:55:00 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64 10:55  up 19 days, 10:22, 2 users, load averages: 3.39 4.48 4.38

--- npc/vsrc/ysyx_lsu.v
+++ npc/vsrc/ysyx_lsu.v
@@ -64,13 +64,12 @@ module ysyx_LSU(
   reg [32-1:0] l1d[L1D_SIZE-1:0];
   reg [L1D_SIZE-1:0] l1d_valid = 0;
   reg [32-L1D_LEN-2-1:0] l1d_tag[L1D_SIZE-1:0];
-  reg l1_enable = 0;
 
   wire arvalid;
   wire [32-L1D_LEN-2-1:0] addr_tag = lsu_araddr_o[ADDR_W-1:L1D_LEN+2];
   wire [L1D_LEN-1:0] addr_idx = lsu_araddr_o[L1D_LEN+2-1:0+2];
   wire l1d_cache_hit = (
-         l1_enable &
+         ren & lsu_avalid &
          l1d_valid[addr_idx] == 1'b1) & (l1d_tag[addr_idx] == addr_tag);
 
   wire [32-L1D_LEN-2-1:0] waddr_tag = lsu_awaddr_o[ADDR_W-1:L1D_LEN+2];
