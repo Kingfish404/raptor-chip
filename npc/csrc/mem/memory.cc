8a81bca3c219f9949082a74dc0726ecf7d27112d
tracer-ysyx <tracer@ysyx.org>
2024-04-28 17:29:36 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.4.0 Darwin Kernel Version 23.4.0: Fri Mar 15 00:10:42 PDT 2024; root:xnu-10063.101.17~1/RELEASE_ARM64_T6000 arm64 17:29  up 26 days,  3:26, 5 users, load averages: 4.98 4.42 4.29

--- npc/csrc/mem/memory.cc
+++ npc/csrc/mem/memory.cc
@@ -170,7 +170,7 @@ extern "C" void flash_read(uint32_t addr, uint32_t *data)
 {
     uint32_t offset = addr;
     *data = *((uint32_t *)(flash + offset));
-    Log("flash raddr: 0x%x, rdata: 0x%x, offest: 0x%x", addr, *data, offset);
+    // Log("flash raddr: 0x%x, rdata: 0x%x, offest: 0x%x", addr, *data, offset);
 }
 
 extern "C" void mrom_read(uint32_t addr, uint32_t *data)
