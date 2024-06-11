400641e10aea34d193718fe685e361e5e174426b
tracer-ysyx <tracer@ysyx.org>
2024-06-11 16:04:05 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 16:04  up 27 days, 16:53, 9 users, load averages: 2.50 3.01 3.39

--- npc/csrc/monitor/cpu/cpu_exec.cc
+++ npc/csrc/monitor/cpu/cpu_exec.cc
@@ -32,8 +32,9 @@ static void perf()
 
   Log(FMT_BLUE("IFU Fetch: %lld, LSU Load: %lld, EXU ALU: %lld"),
       pmu.ifu_fetch_cnt, pmu.lsu_load_cnt, pmu.exu_alu_cnt);
-  Log(FMT_BLUE("LD Inst: %lld, ST Inst: %lld"),
-      pmu.ld_inst_cnt, pmu.st_inst_cnt);
+  Log(FMT_BLUE("LD Inst: %lld (%2.1f%), ST Inst: %lld"),
+      pmu.ld_inst_cnt, 100.0 * pmu.ld_inst_cnt / pmu.instr_cnt,
+      pmu.st_inst_cnt, 100.0 * pmu.st_inst_cnt / pmu.instr_cnt, );
 }
 
 static void perf_sample_per_cycle()
