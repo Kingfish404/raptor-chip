181570d1c0a81c6b7d8532cd61e3cda415cc463c
tracer-ysyx <tracer@ysyx.org>
2024-06-11 16:30:32 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 16:30  up 27 days, 17:19, 9 users, load averages: 3.65 5.26 4.88

--- npc/csrc/monitor/cpu/cpu_exec.cc
+++ npc/csrc/monitor/cpu/cpu_exec.cc
@@ -37,18 +37,18 @@ static void perf()
   Log(FMT_BLUE("#Inst: %lld, Cycle: %llu, IPC: %.3f"), pmu.instr_cnt, pmu.active_cycle, (1.0 * pmu.instr_cnt / pmu.active_cycle));
   Log(FMT_BLUE("IFU Fetch: %8lld, LSU Load: %8lld, EXU ALU: %lld"),
       pmu.ifu_fetch_cnt, pmu.lsu_load_cnt, pmu.exu_alu_cnt);
-  Log(FMT_BLUE("LD  Inst: %8lld (%2.1f%%), ST Inst: %8lld, (%2.1f%%)"),
+  Log(FMT_BLUE("LD  Inst: %8lld (%4.1f%%), ST Inst: %8lld, (%4.1f%%)"),
       pmu.ld_inst_cnt, percentage(pmu.ld_inst_cnt, pmu.instr_cnt),
       pmu.st_inst_cnt, percentage(pmu.st_inst_cnt, pmu.instr_cnt));
-  Log(FMT_BLUE("ALU Inst: %8lld (%2.1f%%), BR Inst: %8lld, (%2.1f%%)"),
+  Log(FMT_BLUE("ALU Inst: %8lld (%4.1f%%), BR Inst: %8lld, (%4.1f%%)"),
       pmu.alu_inst_cnt, percentage(pmu.alu_inst_cnt, pmu.instr_cnt),
       pmu.b_inst_cnt, percentage(pmu.b_inst_cnt, pmu.instr_cnt));
-  Log(FMT_BLUE("CSR Inst: %8lld (%2.1f%%)"),
+  Log(FMT_BLUE("CSR Inst: %8lld (%4.1f%%)"),
       pmu.csr_inst_cnt, percentage(pmu.csr_inst_cnt, pmu.instr_cnt));
-  Log(FMT_BLUE("Other Inst: %8lld (%2.1f%%)"),
+  Log(FMT_BLUE("Oth Inst: %8lld (%4.1f%%)"),
       pmu.other_inst_cnt, percentage(pmu.other_inst_cnt, pmu.instr_cnt));
   printf("======== TOP DOWN Analysis ========\n");
-  Log(FMT_BLUE("IFU Stall: %8lld (%2.1f%%), LSU Stall: %8lld (%2.1f%%)"),
+  Log(FMT_BLUE("IFU Stall: %8lld (%4.1f%%), LSU Stall: %8lld (%4.1f%%)"),
       pmu.ifu_stall_cycle, percentage(pmu.ifu_stall_cycle, pmu.active_cycle),
       pmu.lsu_stall_cycle, percentage(pmu.lsu_stall_cycle, pmu.active_cycle));
   // show average IF cycle and LS cycle
