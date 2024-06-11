733e30b91677b638753298fcf46d8a41097fd617
tracer-ysyx <tracer@ysyx.org>
2024-06-11 19:43:34 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 19:43  up 27 days, 20:32, 9 users, load averages: 8.59 10.69 9.10

--- npc/vsrc/ysyx_dpi_c.v
+++ npc/vsrc/ysyx_dpi_c.v
@@ -9,13 +9,13 @@ import "DPI-C" function void pmem_write (input int waddr, input int wdata, input
 import "DPI-C" function void sdram_read (input int raddr, output byte rdata);
 import "DPI-C" function void sdram_write (input int waddr, input byte wdata, input byte wmask);
 
-`define ysyx_DPI_C_npc_exu_ebreak npc_exu_ebreak();
-`define ysyx_DPI_C_npc_illegal_inst npc_illegal_inst();
-`define ysyx_DPI_C_npc_difftest_skip_ref npc_difftest_skip_ref();
-`define ysyx_DPI_C_npc_difftest_mem_diff npc_difftest_mem_diff();
+// `define ysyx_DPI_C_npc_exu_ebreak npc_exu_ebreak();
+// `define ysyx_DPI_C_npc_illegal_inst npc_illegal_inst();
+// `define ysyx_DPI_C_npc_difftest_skip_ref npc_difftest_skip_ref();
+// `define ysyx_DPI_C_npc_difftest_mem_diff npc_difftest_mem_diff();
 
-`define ysyx_DPI_C_pmem_read(pm_raddr, pm_rdata) pmem_read(pm_raddr, pm_rdata)
-`define ysyx_DPI_C_pmem_write(pm_waddr, pm_wdata, pm_wmask) pmem_write(pm_waddr, pm_wdata, pm_wmask)
+// `define ysyx_DPI_C_pmem_read(pm_raddr, pm_rdata) pmem_read(pm_raddr, pm_rdata)
+// `define ysyx_DPI_C_pmem_write(pm_waddr, pm_wdata, pm_wmask) pmem_write(pm_waddr, pm_wdata, pm_wmask)
 
-`define ysyx_DPI_C_sdram_read(sd_raddr, sd_rdata) sdram_read(sd_raddr, sd_rdata)
-`define ysyx_DPI_C_sdram_write(sd_waddr, sd_wdata, sd_wmask) sdram_write(sd_waddr, sd_wdata, sd_wmask)
+// `define ysyx_DPI_C_sdram_read(sd_raddr, sd_rdata) sdram_read(sd_raddr, sd_rdata)
+// `define ysyx_DPI_C_sdram_write(sd_waddr, sd_wdata, sd_wmask) sdram_write(sd_waddr, sd_wdata, sd_wmask)
