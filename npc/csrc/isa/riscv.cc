259ca773835693feb00f2afc99effd42dce675e5
tracer-ysyx <tracer@ysyx.org>
2024-06-11 15:32:25 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 15:32  up 27 days, 16:21, 9 users, load averages: 3.40 4.89 4.30

--- npc/csrc/isa/riscv.cc
+++ npc/csrc/isa/riscv.cc
@@ -1,6 +1,8 @@
 #include <common.h>
 #include <string.h>
 
+extern NPCState npc;
+
 const char *regs[] = {
     "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
     "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
