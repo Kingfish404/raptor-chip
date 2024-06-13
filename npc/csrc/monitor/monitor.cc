e67b8183370af527cd581826e8c059d1ce41d1d2
tracer-ysyx <tracer@ysyx.org>
2024-06-13 14:20:34 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 14:20  up 29 days, 15:09, 5 users, load averages: 2.01 2.84 3.06

--- npc/csrc/monitor/monitor.cc
+++ npc/csrc/monitor/monitor.cc
@@ -168,7 +168,7 @@ void init_monitor(int argc, char *argv[])
     Log("argv[%d] = %s", i, argv[i]);
   }
   //  -b -n -d /Users/jinyu/Developer/c-project/ysyx-workbench/nemu/build/riscv32-nemu-interpreter-so -m /Users/jinyu/Developer/c-project/ysyx-workbench/npc/csrc/mem/mrom-data/build/mrom-data.bin /Users/jinyu/Developer/c-project/ysyx-workbench/am-kernels/benchmarks/microbench/build/microbench-riscv32e-ysyxsoc.bin
-  int _argc = 9;
+  int _argc = 7;
   char arg1[10] = "-b";
   char arg2[10] = "-n";
   char arg3[10] = "-d";
