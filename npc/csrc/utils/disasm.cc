6db047712af8a89b208ad64615cea9ad6ad3e20f
tracer-ysyx <tracer@ysyx.org>
2024-06-21 10:49:12 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 10:49  up 37 days, 11:38, 4 users, load averages: 4.26 4.15 3.83

--- npc/csrc/utils/disasm.cc
+++ npc/csrc/utils/disasm.cc
@@ -12,7 +12,6 @@
  *
  * See the Mulan PSL v2 for more details.
  ***************************************************************************************/
-#include <common.h>
 
 #if defined(__GNUC__) && !defined(__clang__)
 #pragma GCC diagnostic push
