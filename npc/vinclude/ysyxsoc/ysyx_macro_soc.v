9d8c01d1c6b9030b93abbec6215e9db0774634bb
tracer-ysyx <tracer@ysyx.org>
2024-06-30 14:55:41 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 14:55  up 8 days, 18:42, 3 users, load averages: 4.47 3.75 3.76

--- npc/vinclude/ysyxsoc/ysyx_macro_soc.v
+++ npc/vinclude/ysyxsoc/ysyx_macro_soc.v
@@ -3,3 +3,6 @@
 `define ysyx_BUS_SERIAL_PORT    'h10000000
 `define ysyx_BUS_RTC_ADDR       'h02000048
 `define ysyx_BUS_RTC_ADDR_UP    `ysyx_BUS_RTC_ADDR + 4
+
+// random test setting
+`define ysyx_IFSR_ENABLE 0
