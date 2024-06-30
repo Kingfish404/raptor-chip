cb6298c8e1a297a30ea23121d56b77969ac4bdf6
tracer-ysyx <tracer@ysyx.org>
2024-06-30 17:21:51 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 17:21  up 8 days, 21:08, 3 users, load averages: 6.23 5.14 4.48

--- npc/csrc/mem/memory.cc
+++ npc/csrc/mem/memory.cc
@@ -119,8 +119,8 @@ extern "C" void pmem_read(word_t raddr, word_t *data)
     if (raddr >= MBASE && raddr < MBASE + MSIZE)
     {
         *data = host_read(pmem + raddr - MBASE, 4);
-        Log("raddr: " FMT_WORD_NO_PREFIX ", data: " FMT_WORD_NO_PREFIX,
-            raddr, *data);
+        // Log("raddr: " FMT_WORD_NO_PREFIX ", data: " FMT_WORD_NO_PREFIX,
+        //     raddr, *data);
         return;
     }
     npc_abort();
@@ -129,8 +129,8 @@ extern "C" void pmem_read(word_t raddr, word_t *data)
 
 extern "C" void pmem_write(word_t waddr, word_t wdata, char wmask)
 {
-    Log("waddr: 0x%08x, wdata: 0x%08x, wmask = 0x%08x",
-        waddr, wdata, wmask);
+    // Log("waddr: 0x%08x, wdata: 0x%08x, wmask = 0x%08x",
+    //     waddr, wdata, wmask);
 #ifdef CONFIG_SOFT_MMIO
     // SERIAL_MMIO: hex "MMIO address of the serial controller"
     if (waddr == SERIAL_PORT)
