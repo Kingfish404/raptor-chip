0c868dbad06348617a7407b37fb69ebdb9ed1356
tracer-ysyx <tracer@ysyx.org>
2024-06-30 19:24:28 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 19:24  up 8 days, 23:11, 4 users, load averages: 3.99 4.22 3.87

--- npc/csrc/mem/memory.cc
+++ npc/csrc/mem/memory.cc
@@ -129,8 +129,8 @@ extern "C" void pmem_read(word_t raddr, word_t *data)
 
 extern "C" void pmem_write(word_t waddr, word_t wdata, char wmask)
 {
-    // Log("waddr: 0x%08x, wdata: 0x%08x, wmask = 0x%08x",
-    //     waddr, wdata, wmask);
+    // Log("waddr: 0x%08x, wdata: 0x%08x, wmask = 0x%02x",
+    //     waddr, wdata, wmask & 0xff);
 #ifdef CONFIG_SOFT_MMIO
     // SERIAL_MMIO: hex "MMIO address of the serial controller"
     if (waddr == SERIAL_PORT)
@@ -142,8 +142,8 @@ extern "C" void pmem_write(word_t waddr, word_t wdata, char wmask)
 #endif
     if (waddr < MBASE || waddr > MBASE + MSIZE)
     {
-        Log("Invalid write: addr = " FMT_WORD ", data = " FMT_WORD ", mask = %x",
-            waddr, wdata, wmask);
+        // Log("Invalid write: addr = " FMT_WORD ", data = " FMT_WORD ", mask = %02x",
+        //     waddr, wdata, wmask & 0xff);
         npc_abort();
         return;
     }
@@ -162,6 +162,8 @@ extern "C" void pmem_write(word_t waddr, word_t wdata, char wmask)
     //     host_write(pmem + waddr - MBASE, wdata, 8);
     //     break;
     default:
+        // Log(FMT_RED("Invalid write: addr = " FMT_WORD ", data = " FMT_WORD ", mask = %02x"),
+        //     waddr, wdata, wmask & 0xff);
         break;
     }
 }
