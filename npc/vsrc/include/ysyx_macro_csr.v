74d236b0189a16bc88d2d2192c042e9f55f9ffd9
tracer-ysyx <tracer@ysyx.org>
2024-07-02 20:22:53 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 20:22  up 11 days, 9 mins, 4 users, load averages: 4.94 4.42 4.05

--- npc/vsrc/include/ysyx_macro_csr.v
+++ npc/vsrc/include/ysyx_macro_csr.v
@@ -1,16 +1,16 @@
 
 // Machine Trap Handling
-`define ysyx_CSR_MCAUSE 12'h342
-`define ysyx_CSR_MEPC   12'h341
+`define ysyx_CSR_MCAUSE 'h342
+`define ysyx_CSR_MEPC   'h341
 
 // Machine Trap Settup
-`define ysyx_CSR_MTVEC   12'h305
-`define ysyx_CSR_MSTATUS 12'h300
+`define ysyx_CSR_MTVEC   'h305
+`define ysyx_CSR_MSTATUS 'h300
 
 // CSR_MSTATUS FLAGS
-`define ysyx_CSR_MSTATUS_MPIE_IDX  12'h7
-`define ysyx_CSR_MSTATUS_MIE_IDX   12'h3
+`define ysyx_CSR_MSTATUS_MPIE_IDX  'h7
+`define ysyx_CSR_MSTATUS_MIE_IDX   'h3
 
 // Machine Information Registers
-`define ysyx_CSR_MVENDORID 12'hf11
-`define ysyx_CSR_MARCHID   12'hf12
+`define ysyx_CSR_MVENDORID 'hf11
+`define ysyx_CSR_MARCHID   'hf12
