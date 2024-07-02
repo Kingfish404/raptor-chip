63117ab0ec2f61e43e365f083cfb044ce8f7d7fc
tracer-ysyx <tracer@ysyx.org>
2024-07-02 20:12:15 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 20:12  up 10 days, 23:59, 4 users, load averages: 2.94 3.16 3.70

--- npc/vsrc/ysyx_exu_csr.v
+++ npc/vsrc/ysyx_exu_csr.v
@@ -22,6 +22,20 @@ module ysyx_CSR_Reg(
   parameter RESET_VAL = 0;
 
   reg [BIT_W-1:0] csr[5];
+  wire [R_W-1:0] waddr_reg_1 = (
+    {{R_W{waddr==`ysyx_CSR_MSTATUS}} & (MCAUSE_IDX)} |
+    {{R_W{waddr==`ysyx_CSR_MCAUSE}} & (MEPC_IDX)} |
+    {{R_W{waddr==`ysyx_CSR_MEPC}} & MTVEC_IDX} |
+    {{R_W{waddr==`ysyx_CSR_MTVEC}} & MSTATUS_IDX} |
+    (MNONE)
+  );
+  wire [R_W-1:0] waddr_reg_2 = (
+    {{R_W{waddr_add1==`ysyx_CSR_MSTATUS}} & (MCAUSE_IDX)} |
+    {{R_W{waddr_add1==`ysyx_CSR_MCAUSE}} & (MEPC_IDX)} |
+    {{R_W{waddr_add1==`ysyx_CSR_MEPC}} & MTVEC_IDX} |
+    {{R_W{waddr_add1==`ysyx_CSR_MTVEC}} & MSTATUS_IDX} |
+    (MNONE)
+  );
 
   assign rdata_o = (
            ({BIT_W{waddr==`ysyx_CSR_MVENDORID}}) & (32'h79737978) |
@@ -35,61 +49,64 @@ module ysyx_CSR_Reg(
   assign mepc_o  = csr[MEPC_IDX];
   assign mtvec_o = csr[MTVEC_IDX];
 
-  // always @(posedge clk)
-  //   begin
-  //     if (rst)
-  //       begin
-  //         csr[MCAUSE_IDX]   <= RESET_VAL;
-  //         csr[MEPC_IDX]     <= RESET_VAL;
-  //         csr[MTVEC_IDX]    <= RESET_VAL;
-  //         csr[MSTATUS_IDX]  <= RESET_VAL;
-  //       end
-  //     else if (exu_valid)
-  //       begin
-  //         if (wen)
-  //           begin
-  //             case (waddr)
-  //               `ysyx_CSR_MSTATUS:
-  //                 begin
-  //                   csr[MSTATUS_IDX] <= wdata;
-  //                 end
-  //               `ysyx_CSR_MEPC:
-  //                 begin
-  //                   csr[MEPC_IDX] <= wdata;
-  //                 end
-  //               `ysyx_CSR_MTVEC:
-  //                 begin
-  //                   csr[MTVEC_IDX] <= wdata;
-  //                 end
-  //               `ysyx_CSR_MCAUSE:
-  //                 begin
-  //                   csr[MCAUSE_IDX] <= wdata;
-  //                 end
-  //             endcase
-  //             case (waddr_add1)
-  //               `ysyx_CSR_MSTATUS:
-  //                 begin
-  //                   csr[MSTATUS_IDX] <= wdata_add1;
-  //                 end
-  //               `ysyx_CSR_MEPC:
-  //                 begin
-  //                   csr[MEPC_IDX] <= wdata_add1;
-  //                 end
-  //               `ysyx_CSR_MTVEC:
-  //                 begin
-  //                   csr[MTVEC_IDX] <= wdata_add1;
-  //                 end
-  //               `ysyx_CSR_MCAUSE:
-  //                 begin
-  //                   csr[MCAUSE_IDX] <= wdata_add1;
-  //                 end
-  //             endcase
-  //           end
-  //         if (ecallen)
-  //           begin
-  //             csr[MSTATUS_IDX][`ysyx_CSR_MSTATUS_MPIE_IDX] <= csr[MSTATUS_IDX][`ysyx_CSR_MSTATUS_MIE_IDX];
-  //             csr[MSTATUS_IDX][`ysyx_CSR_MSTATUS_MIE_IDX] <= 1'b0;
-  //           end
-  //       end
-  //   end
+  always @(posedge clk)
+    begin
+      if (rst)
+        begin
+          csr[MNONE]        <= RESET_VAL;
+          csr[MCAUSE_IDX]   <= RESET_VAL;
+          csr[MEPC_IDX]     <= RESET_VAL;
+          csr[MTVEC_IDX]    <= RESET_VAL;
+          csr[MSTATUS_IDX]  <= RESET_VAL;
+        end
+      else if (exu_valid)
+        begin
+          if (wen)
+            begin
+              csr[waddr_reg_1] <= wdata;
+              csr[waddr_reg_2] <= wdata_add1;
+              // case (waddr)
+              //   `ysyx_CSR_MSTATUS:
+              //     begin
+              //       csr[MSTATUS_IDX] <= wdata;
+              //     end
+              //   `ysyx_CSR_MEPC:
+              //     begin
+              //       csr[MEPC_IDX] <= wdata;
+              //     end
+              //   `ysyx_CSR_MTVEC:
+              //     begin
+              //       csr[MTVEC_IDX] <= wdata;
+              //     end
+              //   `ysyx_CSR_MCAUSE:
+              //     begin
+              //       csr[MCAUSE_IDX] <= wdata;
+              //     end
+              // endcase
+              // case (waddr_add1)
+              //   `ysyx_CSR_MSTATUS:
+              //     begin
+              //       csr[MSTATUS_IDX] <= wdata_add1;
+              //     end
+              //   `ysyx_CSR_MEPC:
+              //     begin
+              //       csr[MEPC_IDX] <= wdata_add1;
+              //     end
+              //   `ysyx_CSR_MTVEC:
+              //     begin
+              //       csr[MTVEC_IDX] <= wdata_add1;
+              //     end
+              //   `ysyx_CSR_MCAUSE:
+              //     begin
+              //       csr[MCAUSE_IDX] <= wdata_add1;
+              //     end
+              // endcase
+            end
+          if (ecallen)
+            begin
+              csr[MSTATUS_IDX][`ysyx_CSR_MSTATUS_MPIE_IDX] <= csr[MSTATUS_IDX][`ysyx_CSR_MSTATUS_MIE_IDX];
+              csr[MSTATUS_IDX][`ysyx_CSR_MSTATUS_MIE_IDX] <= 1'b0;
+            end
+        end
+    end
 endmodule //ysyx_CSR_Reg
