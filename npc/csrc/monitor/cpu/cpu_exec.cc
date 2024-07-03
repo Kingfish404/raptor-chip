d74c7351801209443468d0b2950120a7c9e76769
tracer-ysyx <tracer@ysyx.org>
2024-07-03 18:11:53 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 18:11  up 11 days, 21:58, 4 users, load averages: 4.53 3.48 2.88

--- npc/csrc/monitor/cpu/cpu_exec.cc
+++ npc/csrc/monitor/cpu/cpu_exec.cc
@@ -89,13 +89,15 @@ static void perf()
        pmu.csr_inst_cnt +
        pmu.other_inst_cnt));
   printf("======== Cache Analysis ========\n");
-  printf("| %8s, %% | %8s, %% | %8s, %% | %8s, %% |\n",
-         "HIT", "MISS", "HIT CYC", "MISS CYC");
-  printf("| %8lld,%2.0f | %8lld,%2.0f | %8lld,%2.0f | %8lld,%2.0f |\n",
+  printf("| %8s, %% | %8s, %% | %8s, %% | %8s, %% | %8s | %8s |\n",
+         "HIT", "MISS", "HIT CYC", "MISS CYC", "HIT AVG", "MISS AVG");
+  printf("| %8lld,%2.0f | %8lld,%2.0f | %8lld,%2.0f | %8lld,%2.0f | %8lld | %8lld |\n",
          pmu.l1i_cache_hit_cnt, percentage(pmu.l1i_cache_hit_cnt, pmu.l1i_cache_hit_cnt + pmu.l1i_cache_miss_cnt),
          pmu.l1i_cache_miss_cnt, percentage(pmu.l1i_cache_miss_cnt, pmu.l1i_cache_hit_cnt + pmu.l1i_cache_miss_cnt),
          pmu.l1i_cache_hit_cycle, percentage(pmu.l1i_cache_hit_cycle, pmu.l1i_cache_hit_cycle + pmu.l1i_cache_miss_cycle),
-         pmu.l1i_cache_miss_cycle, percentage(pmu.l1i_cache_miss_cycle, pmu.l1i_cache_hit_cycle + pmu.l1i_cache_miss_cycle));
+         pmu.l1i_cache_miss_cycle, percentage(pmu.l1i_cache_miss_cycle, pmu.l1i_cache_hit_cycle + pmu.l1i_cache_miss_cycle),
+         pmu.l1i_cache_hit_cycle / (pmu.l1i_cache_hit_cnt + 1),
+         pmu.l1i_cache_miss_cycle / (pmu.l1i_cache_miss_cnt + 1));
   assert(
       (pmu.l1i_cache_hit_cnt + pmu.l1i_cache_miss_cnt) == pmu.ifu_fetch_cnt);
 }
