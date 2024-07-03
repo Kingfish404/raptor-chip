d6e54182136c407a778f8872886b7ca08956bdc2
tracer-ysyx <tracer@ysyx.org>
2024-07-03 21:32:31 +0800

    > compile NEMU ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 21:32  up 12 days,  1:19, 4 users, load averages: 1.99 2.15 2.07

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -73,12 +73,10 @@ word_t paddr_read(paddr_t addr, int len) {
 
   if (likely(
     in_pmem(addr)
-#ifdef CONFIG_TARGET_SHARE
     || in_sdram(addr)
     || in_sram(addr)
     || in_mrom(addr)
     || in_flash(addr)
-#endif
     )) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -92,10 +90,8 @@ void paddr_write(paddr_t addr, int len, word_t data) {
 
   if (likely(
     in_pmem(addr)
-#ifdef CONFIG_TARGET_SHARE
     || in_sdram(addr)
     || in_sram(addr)
-#endif
     )) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
