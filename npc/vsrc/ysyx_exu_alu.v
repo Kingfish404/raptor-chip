4659e734ba0636059754804c2446ef9b6739cb8b
tracer-ysyx <tracer@ysyx.org>
2024-07-03 10:44:11 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 10:44  up 11 days, 14:31, 4 users, load averages: 4.05 4.26 4.70

--- npc/vsrc/ysyx_exu_alu.v
+++ npc/vsrc/ysyx_exu_alu.v
@@ -4,7 +4,7 @@ module ysyx_ALU (
   input [BIT_W-1:0] alu_src1,
   input [BIT_W-1:0] alu_src2,
   input [3:0] alu_op,
-  output [BIT_W-1:0] alu_res_o
+  output reg [BIT_W-1:0] alu_res_o
 );
   parameter integer BIT_W = `ysyx_W_WIDTH;
 
