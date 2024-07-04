22e2956fade6d44be26701644432602141e5b5ba
tracer-ysyx <tracer@ysyx.org>
2024-07-04 17:41:56 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 17:41  up 12 days, 21:28, 4 users, load averages: 1.82 2.03 2.59

--- npc/vsrc/ysyx_ifu.v
+++ npc/vsrc/ysyx_ifu.v
@@ -25,15 +25,17 @@ module ysyx_IFU (
   assign ready_o = !valid_o;
   assign arvalid = pvalid;
 
+  parameter integer L1I_LINE_SIZE = 32;
   parameter integer L1I_SIZE = 8;
   parameter integer L1I_LEN = 3;
-  reg [32-1:0] l1i[L1I_SIZE];
+  reg [L1I_LINE_SIZE-1:0] l1i[L1I_SIZE];
   reg [L1I_SIZE-1:0] l1i_valid = 0;
   reg [32-L1I_LEN-2-1:0] l1i_tag[L1I_SIZE];
 
   wire arvalid;
   wire [32-L1I_LEN-2-1:0] addr_tag = ifu_araddr_o[ADDR_W-1:L1I_LEN+2];
   wire [L1I_LEN-1:0] addr_idx = ifu_araddr_o[L1I_LEN+2-1:0+2];
+  wire addr_offset = ifu_araddr_o[1:0];
   wire l1i_cache_hit = (
          (pvalid) & 1 &
          l1i_valid[addr_idx] == 1'b1) & (l1i_tag[addr_idx] == addr_tag);
