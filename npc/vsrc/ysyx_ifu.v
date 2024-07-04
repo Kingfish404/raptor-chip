d8d8bd3d36d9d9415408377526e3b96bbe183e90
tracer-ysyx <tracer@ysyx.org>
2024-07-04 18:44:22 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 18:44  up 12 days, 22:31, 4 users, load averages: 4.71 4.35 3.57

--- npc/vsrc/ysyx_ifu.v
+++ npc/vsrc/ysyx_ifu.v
@@ -19,36 +19,31 @@ module ysyx_IFU (
   parameter integer ADDR_W = 32;
   parameter integer DATA_W = 32;
 
-  reg state;
+  reg state, valid;
   reg pvalid;
 
   assign ready_o = !valid_o;
   assign arvalid = pvalid;
 
-  parameter integer L1I_LINE_SIZE = 2;
-  parameter integer L1I_LINE_LEN = 1;
   parameter integer L1I_SIZE = 8;
   parameter integer L1I_LEN = 3;
-  reg [32-1:0] l1i[L1I_SIZE][L1I_LINE_SIZE];
+  reg [32-1:0] l1i[L1I_SIZE];
   reg [L1I_SIZE-1:0] l1i_valid = 0;
-  reg [32-L1I_LEN-L1I_LINE_LEN-2-1:0] l1i_tag[L1I_SIZE];
-  reg [1:0] l1i_state = 0;
+  reg [32-L1I_LEN-2-1:0] l1i_tag[L1I_SIZE];
 
   wire arvalid;
-
-  wire [32-L1I_LEN-L1I_LINE_LEN-2-1:0] addr_tag = ifu_araddr_o[ADDR_W-1:L1I_LEN+L1I_LINE_LEN+2];
-  wire [L1I_LEN-1:0] addr_idx = ifu_araddr_o[L1I_LEN+L1I_LINE_LEN+2-1:L1I_LINE_LEN+2];
-  wire [L1I_LINE_LEN-1:0]addr_offset = ifu_araddr_o[L1I_LINE_LEN+2-1:2];
-  reg l1i_cache_hit = (
+  wire [32-L1I_LEN-2-1:0] addr_tag = ifu_araddr_o[ADDR_W-1:L1I_LEN+2];
+  wire [L1I_LEN-1:0] addr_idx = ifu_araddr_o[L1I_LEN+2-1:0+2];
+  wire l1i_cache_hit = (
          (pvalid) & 1 &
          l1i_valid[addr_idx] == 1'b1) & (l1i_tag[addr_idx] == addr_tag);
 
-  assign ifu_araddr_o = (l1i_state == 'b00 || l1i_state == 'b01) ? pc & ~'h4 : pc | ~'h4;
+  assign ifu_araddr_o = pc;
   assign ifu_arvalid_o = arvalid & !l1i_cache_hit;
 
   // with l1i cache
-  assign inst_o =  l1i[addr_idx][addr_offset];
-  assign valid_o = l1i_cache_hit;
+  assign inst_o = (ifu_rvalid) ? ifu_rdata : l1i[addr_idx];
+  assign valid_o = ifu_rvalid | l1i_cache_hit;
 
   `ysyx_BUS_FSM()
   assign pc_o = pc;
@@ -56,53 +51,29 @@ module ysyx_IFU (
     begin
       if (rst)
         begin
+          valid <= 0;
           pvalid <= 1;
-          l1i_state <= 0;
         end
       else
         begin
-          if (valid_o && inst_o == 'h0000100f) begin // fence.i
-            l1i_valid <= 0;
-          end
-          case (l1i_state)
-            'b00:
-              if (ifu_arvalid_o)
-                begin
-                  l1i_state <= 'b01;
-                end
-            'b01:
-              if (ifu_rvalid)
-                  begin
-                    l1i_state <= 'b10;
-                    l1i[addr_idx][0] <= ifu_rdata;
-                  end
-            'b10:
-              if (ifu_rvalid)
-                  begin
-                    l1i_state <= 'b00;
-                    l1i[addr_idx][1] <= ifu_rdata;
-                    l1i_tag[addr_idx] <= addr_tag;
-                    l1i_valid[addr_idx] <= 1'b1;
-                  end
-            'b11:
-              begin
+          if (ifu_rvalid)
+            begin
+              l1i[addr_idx] <= ifu_rdata;
+              l1i_tag[addr_idx] <= addr_tag;
+              l1i_valid[addr_idx] <= 1'b1;
+              if (ifu_rdata == 'h0000100f) begin
+                l1i_valid <= 0;
               end
-          endcase
-
-          // if (ifu_rvalid)
-          //   begin
-          //     l1i[addr_idx] <= ifu_rdata;
-          //     l1i_tag[addr_idx] <= addr_tag;
-          //     l1i_valid[addr_idx] <= 1'b1;
-          //     if (ifu_rdata == 'h0000100f) begin
-          //       l1i_valid <= 0;
-          //     end
-          //   end
+            end
           if (state == `ysyx_IDLE)
             begin
               if (prev_valid)
                 begin
                   pvalid <= prev_valid;
+                end
+              if (ifu_rvalid | (l1i_cache_hit))
+                begin
+                  valid <= 1;
                 end
             end
           else if (state == `ysyx_WAIT_READY)
@@ -110,6 +81,7 @@ module ysyx_IFU (
               if (next_ready == 1)
                 begin
                   pvalid <= 0;
+                  valid <= 0;
                 end
             end
         end
