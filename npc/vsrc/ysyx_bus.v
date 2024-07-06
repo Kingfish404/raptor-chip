d5621ced802a1a48f3c86e00f3c00bd76128d640
tracer-ysyx <tracer@ysyx.org>
2024-07-07 03:19:08 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64  3:19  up 15 days,  7:06, 5 users, load averages: 6.70 5.73 4.92

--- npc/vsrc/ysyx_bus.v
+++ npc/vsrc/ysyx_bus.v
@@ -159,6 +159,7 @@ module ysyx_BUS_ARBITER(
                 if (io_master_wready)
                   begin
                     write_valid <= 0;
+                    awrite_valid <= 0;
                   end
                 if (io_master_bvalid)
                   begin
