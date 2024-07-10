c900652b3dbf824abacf3733b0e8febd9ddd7507
tracer-ysyx <tracer@ysyx.org>
2024-07-10 17:35:01 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 17:35  up 18 days, 21:22, 3 users, load averages: 3.95 4.95 5.00

--- npc/vsrc/ysyx_idu.v
+++ npc/vsrc/ysyx_idu.v
@@ -62,7 +62,6 @@ module ysyx_IDU (
   );
   assign wen_o = (opcode_o == `ysyx_OP_S_TYPE);
   assign ren_o = (opcode_o == `ysyx_OP_IL_TYPE);
-  assign rd_o = rd;
   always @(*) begin
     rwen_o = 0;
     alu_op_o = 0;
