fa68761029bf8b59bfb8ce72690e91ddfc30aca0
tracer-ysyx <tracer@ysyx.org>
2024-07-10 16:24:40 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 16:24  up 18 days, 20:11, 3 users, load averages: 4.00 3.20 3.61

--- npc/vsrc/ysyx.v
+++ npc/vsrc/ysyx.v
@@ -286,7 +286,7 @@ module ysyx (
     .npc_wdata(npc_wdata),
     .use_exu_npc(use_exu_npc),
 
-    .reg_wdata_o(reg_wdata),
+    .reg_wdata_o(reg_wdata_wbu),
     .rd_o(rd_wbu),
     .npc_wdata_o(npc_wbu),
     .use_exu_npc_o(use_exu_npc_wbu),
