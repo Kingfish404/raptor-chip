6f3ec7bff57cf9f8a9a0accce06d89fae646c473
tracer-ysyx <tracer@ysyx.org>
2024-07-11 12:32:55 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 12:32  up 19 days, 16:19, 3 users, load averages: 3.53 3.30 3.05

--- npc/csrc/monitor/cpu/cpu_exec.cc
+++ npc/csrc/monitor/cpu/cpu_exec.cc
@@ -283,7 +283,6 @@ void cpu_exec(uint64_t n)
   while (!contextp->gotFinish() && npc.state == NPC_RUNNING && n-- > 0)
   {
     prev_pc = *(npc.npc);
-    printf("prev_pc: " FMT_WORD_NO_PREFIX "\n", prev_pc);
     cpu_exec_one_cycle();
     // Simulate the performance monitor unit
     perf_sample_per_cycle();
