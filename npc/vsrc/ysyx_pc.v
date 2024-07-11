305872fec47dc25027e8adf93a105bafce74d502
tracer-ysyx <tracer@ysyx.org>
2024-07-11 11:42:05 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 11:42  up 19 days, 15:29, 3 users, load averages: 2.12 2.67 3.26

--- npc/vsrc/ysyx_pc.v
+++ npc/vsrc/ysyx_pc.v
@@ -12,16 +12,19 @@ module ysyx_pc (
     output reg [DATA_W-1:0] pc_o
 );
   parameter integer DATA_W = `ysyx_W_WIDTH;
+  reg  [DATA_W-1:0] pc;
   wire [DATA_W-1:0] npc = pc_o + 4;
+  assign pc_o = pc;
+
   always @(posedge clk) begin
     if (rst) begin
-      pc_o <= `ysyx_PC_INIT;
+      pc <= `ysyx_PC_INIT;
       `ysyx_DPI_C_npc_difftest_skip_ref
     end else if (prev_valid) begin
       if (use_exu_npc) begin
-        pc_o <= npc_wdata;
+        pc <= npc_wdata;
       end else begin
-        pc_o <= npc;
+        pc <= npc;
       end
     end
   end
