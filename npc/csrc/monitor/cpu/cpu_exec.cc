f1b563bbcefdfcdfec39cdd52d8c34070cf8758d
tracer-ysyx <tracer@ysyx.org>
2024-07-11 13:48:49 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 13:48  up 19 days, 17:35, 3 users, load averages: 2.98 3.90 4.18

--- npc/csrc/monitor/cpu/cpu_exec.cc
+++ npc/csrc/monitor/cpu/cpu_exec.cc
@@ -115,10 +115,11 @@ static void perf_sample_per_cycle()
   }
   pmu.active_cycle++;
   bool ifu_valid = *(uint8_t *)&(CONCAT(VERILOG_PREFIX, __DOT__ifu__DOT__l1i_cache_hit));
+  bool idu_valid = *(uint8_t *)&(CONCAT(VERILOG_PREFIX, __DOT__idu_valid));
   bool ifu_pvalid = *(uint8_t *)&(CONCAT(VERILOG_PREFIX, __DOT__ifu__DOT__pvalid));
   bool l1i_cache_hit = *(uint8_t *)&(CONCAT(VERILOG_PREFIX, __DOT__ifu__DOT__l1i_cache_hit));
   bool lsu_valid = *(uint8_t *)&(CONCAT(VERILOG_PREFIX, __DOT__exu__DOT__lsu_valid));
-  if (ifu_valid)
+  if (ifu_valid & !idu_valid)
   {
     pmu.ifu_fetch_cnt++;
   }
