9355ef2d57b654d38087a0689884ea95528de369
tracer-ysyx <tracer@ysyx.org>
2024-07-11 11:23:47 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 11:23  up 19 days, 15:10, 3 users, load averages: 2.71 3.04 3.45

--- npc/csrc/monitor/cpu/difftest_dut.cc
+++ npc/csrc/monitor/cpu/difftest_dut.cc
@@ -154,7 +154,7 @@ void difftest_step(vaddr_t pc)
 
   if (is_skip_ref)
   {
-    printf("Skip ref at pc = %x\n", pc);
+    printf("Hit Skip ref at pc = %x\n", pc);
     ref_difftest_regcpy(&npc, DIFFTEST_TO_REF);
     is_skip_ref = false;
     return;
