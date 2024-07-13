c633114ae91a080aac855ce3a503054abf9f2e16
tracer-ysyx <tracer@ysyx.org>
2024-07-13 23:28:31 +0800

    > compile NEMU ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 23:28  up 22 days,  3:15, 4 users, load averages: 3.76 3.91 3.94

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -41,8 +41,8 @@ void init_proc()
   // context_kload(&pcb[1], hello_fun, "pcb[1]");
   // context_kload(&pcb[2], hello_fun, "pcb[1]");
   context_uload(&pcb[0], "/bin/dummy");
-  context_uload(&pcb[1], "/bin/dummy");
-  last = &pcb[1];
+  // context_uload(&pcb[1], "/bin/dummy");
+  last = &pcb[0];
   // switch_boot_pcb();
   Log("Initializing processes...");
   yield();
