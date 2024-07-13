5141822e9f508c7c5ae644f94589b2aab94fb322
tracer-ysyx <tracer@ysyx.org>
2024-07-13 22:53:52 +0800

    > compile NEMU ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 22:53  up 22 days,  2:40, 4 users, load averages: 4.66 4.80 4.37

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -36,7 +36,7 @@ void context_uload(PCB *pcb, const char *filename)
 {
   void *entry = ucontext_load(pcb, filename);
   pcb->cp = ucontext(NULL, (Area){pcb->stack, pcb->stack + STACK_SIZE}, entry);
-  pcb->cp->GPRx = (uintptr_t)pcb->stack + STACK_SIZE;
+  pcb->cp->GPRx = (uintptr_t)pcb->stack;
   printf("GPRx: %x\n", pcb->cp->GPRx);
 }
 
