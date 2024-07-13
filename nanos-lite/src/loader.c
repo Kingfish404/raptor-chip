f4293155d7dba89cfec205963519ea2dd9a04304
tracer-ysyx <tracer@ysyx.org>
2024-07-13 16:10:07 +0800

    > compile NEMU ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.5.0 Darwin Kernel Version 23.5.0: Wed May  1 20:12:58 PDT 2024; root:xnu-10063.121.3~5/RELEASE_ARM64_T6000 arm64 16:10  up 21 days, 19:57, 3 users, load averages: 3.30 2.62 2.86

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -49,5 +49,5 @@ void naive_uload(PCB *pcb, const char *filename)
 {
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
-  // ((void (*)())entry)();
+  ((void (*)())entry)();
 }
