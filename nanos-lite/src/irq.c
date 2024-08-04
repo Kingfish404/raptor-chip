3cc17817e53637ad8b621aa0f1f3da50a6673dd2
tracer-ysyx <tracer@ysyx.org>
2024-08-04 16:01:06 +0800

    > compile NEMU ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Fri Jul  5 17:56:41 PDT 2024; root:xnu-10063.141.1~2/RELEASE_ARM64_T6000 arm64 16:01  up 42 mins, 4 users, load averages: 2.25 3.21 5.78

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,22 +4,26 @@ extern Context *schedule(Context *prev);
 
 void do_syscall(Context *c);
 
-static Context* do_event(Event e, Context* c) {
-  switch (e.event) {
-    case EVENT_YIELD:
-      Log("EVENT_YIELD");
-      c = schedule(c);
-      break;
-    case EVENT_SYSCALL:
-      // Log("EVENT_SYSCALL");
-      do_syscall(c);
-      break;
-    default: panic("Unhandled event ID = %d", e.event);
+static Context *do_event(Event e, Context *c)
+{
+  switch (e.event)
+  {
+  case EVENT_YIELD:
+    Log("EVENT_YIELD");
+    c = schedule(c);
+    break;
+  case EVENT_SYSCALL:
+    // Log("EVENT_SYSCALL");
+    do_syscall(c);
+    break;
+  default:
+    panic("Unhandled event ID = %d", e.event);
   }
   return c;
 }
 
-void init_irq(void) {
+void init_irq(void)
+{
   Log("Initializing interrupt/exception handler...");
   cte_init(do_event);
 }
