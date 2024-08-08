6bac55e9e0691bc34830bb3d569420f917907d93
tracer-ysyx <tracer@ysyx.org>
2024-08-08 16:57:05 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Fri Jul  5 17:56:41 PDT 2024; root:xnu-10063.141.1~2/RELEASE_ARM64_T6000 arm64 16:57  up 4 days,  1:38, 4 users, load averages: 1.45 2.81 3.23

--- npc/csrc/monitor/cpu/difftest_dut.cc
+++ npc/csrc/monitor/cpu/difftest_dut.cc
@@ -155,14 +155,12 @@ void difftest_step(vaddr_t pc)
   if (is_skip_ref)
   {
     ref_difftest_regcpy(&npc, DIFFTEST_TO_REF);
-    ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
     is_skip_ref = false;
     return;
   }
 
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
-  Log("diff at pc = " FMT_WORD_NO_PREFIX " ref pc = " FMT_WORD_NO_PREFIX, pc, *(ref_r.pc));
   checkregs(&ref_r, pc);
 
 #ifdef CONFIG_MEM_DIFFTEST
