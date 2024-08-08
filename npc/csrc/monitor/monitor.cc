977c0624b562afa2601cc056102e2a905f42439e
tracer-ysyx <tracer@ysyx.org>
2024-08-08 22:37:51 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Fri Jul  5 17:56:41 PDT 2024; root:xnu-10063.141.1~2/RELEASE_ARM64_T6000 arm64 22:37  up 4 days,  7:19, 4 users, load averages: 3.67 3.74 4.23

--- npc/csrc/monitor/monitor.cc
+++ npc/csrc/monitor/monitor.cc
@@ -28,8 +28,7 @@ static const uint32_t img_char_test[] = {
     0x0080016f, // 80000004: jal sp, 0x8
     0x04100713, // 80000008: addi a4, zero, 0x41
     0x04100713, // 8000000c: addi a4, zero, 0x41
-    0xfe0518e3, //         : bne a0, x0, -16
-    0x100007b7, // 80000010: lui a5, 0x10000
+    0xfe0518e3, // 80000010: bne a0, x0, -16
     0x00000117, // 80000014: auipc sp,0x0
     0x00012483, // 80000018: lw	s1,0(sp)
     0x04100713, // 8000001c: addi a4, zero, 0x41
@@ -37,9 +36,9 @@ static const uint32_t img_char_test[] = {
     0x00000117, // 80000024: auipc sp,0x0
     0x00a00713, // 80000028: addi a4, zero, 0x0a
     0x00100073, // 8000002c: ebreak
-    0x00a00713, // 8000002c: addi a4, zero, 0x0a
-    0x00a00713, // 8000002c: addi a4, zero, 0x0a
-    0x00a00713, // 8000002c: addi a4, zero, 0x0a
+    0x00a00713, // 80000030: addi a4, zero, 0x0a
+    0x00a00713, // 80000034: addi a4, zero, 0x0a
+    0x00a00713, // 80000038: addi a4, zero, 0x0a
 };
 
 void isa_parser_elf(char *filename);
