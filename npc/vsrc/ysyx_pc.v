9bf7ecfe864ec0927cd4c6c2870201e80c288713
tracer-ysyx <tracer@ysyx.org>
2024-08-08 22:14:07 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Fri Jul  5 17:56:41 PDT 2024; root:xnu-10063.141.1~2/RELEASE_ARM64_T6000 arm64 22:14  up 4 days,  6:55, 4 users, load averages: 5.46 5.02 4.53

--- npc/vsrc/ysyx_pc.v
+++ npc/vsrc/ysyx_pc.v
@@ -6,10 +6,12 @@ module ysyx_pc (
     input clk,
     input rst,
     input prev_valid,
-    input use_exu_npc, branch_retire,
+    input use_exu_npc,
+    branch_retire,
     input [DATA_W-1:0] npc_wdata,
     output wire [DATA_W-1:0] npc_o,
-    output reg valid_o, skip_o,
+    output reg valid_o,
+    skip_o,
     output [DATA_W-1:0] pc_o
 );
   parameter integer DATA_W = `ysyx_W_WIDTH;
@@ -28,13 +30,14 @@ module ysyx_pc (
         pc <= npc_wdata;
         valid_o <= 1;
         skip_o <= 0;
-      end else if (branch_retire) begin
-        // pc <= npc;
-        valid_o <= 0;
-        skip_o <= 1;
       end
+      //  else if (branch_retire) begin
+      //   // pc <= npc;
+      //   valid_o <= 0;
+      //   skip_o  <= 1;
+      // end
     end else begin
-      skip_o <= 0;
+      skip_o  <= 0;
       valid_o <= 0;
     end
   end
