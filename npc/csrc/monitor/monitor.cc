09d80bbc67175ec7a1b8c4b81fbe733c97f97519
tracer-ysyx <tracer@ysyx.org>
2024-08-08 16:30:49 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Fri Jul  5 17:56:41 PDT 2024; root:xnu-10063.141.1~2/RELEASE_ARM64_T6000 arm64 16:30  up 4 days,  1:12, 4 users, load averages: 3.12 3.45 3.50

--- npc/csrc/monitor/monitor.cc
+++ npc/csrc/monitor/monitor.cc
@@ -6,7 +6,7 @@
 #include <string.h>
 
 static const uint32_t img[] = {
-    0x00108093, // 80000000: addi ra, ra, 1
+    1, // 80000000: addi ra, ra, 1
     0x00193, // 80000004: addi ra, ra, 1
     0x00108093, // 80000008: addi ra, ra, 1
     0x00108093, // 8000000c: addi ra, ra, 1
