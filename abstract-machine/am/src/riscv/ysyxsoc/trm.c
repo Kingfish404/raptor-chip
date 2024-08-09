0cc9334dc331acc5ae83af11c32c8955598d744f
tracer-ysyx <tracer@ysyx.org>
2024-08-09 23:55:16 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Fri Jul  5 17:56:41 PDT 2024; root:xnu-10063.141.1~2/RELEASE_ARM64_T6000 arm64 23:55  up 5 days,  8:37, 3 users, load averages: 2.22 2.48 2.60

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -63,7 +63,7 @@ void halt(int code)
 
 __attribute__((section(".first_boot"))) void _first_stage_bootloader(void)
 {
-  volatile uint8_t *data = 0x80000010;
+  volatile uint8_t *data = 0x0f001ff8;
   volatile uint32_t d = 1;
   for (volatile int i = 0; i < 2; i++)
   {
