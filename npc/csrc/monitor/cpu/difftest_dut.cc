66961451284acd7c01120cf9fbef6a02b1577f76
tracer-ysyx <tracer@ysyx.org>
2024-08-11 20:36:57 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Fri Jul  5 17:56:41 PDT 2024; root:xnu-10063.141.1~2/RELEASE_ARM64_T6000 arm64 20:36  up 7 days,  5:18, 3 users, load averages: 3.09 3.14 3.30

--- npc/csrc/monitor/cpu/difftest_dut.cc
+++ npc/csrc/monitor/cpu/difftest_dut.cc
@@ -86,13 +86,12 @@ static void checkregs(NPCState *ref, vaddr_t pc)
            (vaddr_t)(*(ref->pc)), pc);
     is_same = false;
   }
-  // can't compare inst for pipeline cpu
-  // if ((uint32_t)(*(ref->inst)) != npc.last_inst)
-  // {
-  //   printf(FMT_RED("[ERROR]") "    inst is different! ref = " FMT_WORD_NO_PREFIX ", dut = " FMT_WORD_NO_PREFIX "\n",
-  //          *(ref->inst), npc.last_inst);
-  //   is_same = false;
-  // }
+  if ((uint32_t)(*(ref->inst)) != *npc.inst)
+  {
+    printf(FMT_RED("[ERROR]") "    inst is different! ref = " FMT_WORD_NO_PREFIX ", dut = " FMT_WORD_NO_PREFIX "\n",
+           *(ref->inst), *npc.inst);
+    is_same = false;
+  }
   for (int i = 0; i < GPR_SIZE; i++)
   {
     if (ref->gpr[i] != npc.gpr[i])
