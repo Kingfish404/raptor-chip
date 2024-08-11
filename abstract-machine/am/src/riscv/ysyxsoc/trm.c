d348234258dbab6d830763560f449a768fef46c8
tracer-ysyx <tracer@ysyx.org>
2024-08-12 00:02:30 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Fri Jul  5 17:56:41 PDT 2024; root:xnu-10063.141.1~2/RELEASE_ARM64_T6000 arm64  0:02  up 7 days,  8:44, 3 users, load averages: 5.31 4.77 4.53

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -77,7 +77,6 @@ __attribute__((section(".first_boot"))) void _first_stage_bootloader(void)
       _second_boot_start[i] = _second_boot_load_start[i];
     }
   }
-  // read the second stage bootloader from flash
   for (uintptr_t p = (uintptr_t)_second_boot_start; p < (uintptr_t)_second_boot_end; p += 4)
   {
     volatile uint32_t data = *(volatile uint32_t *)p;
