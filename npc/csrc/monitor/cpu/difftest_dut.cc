3e3ea1b6e1ac74c52d28ced17a34d3978b44baa9
tracer-ysyx <tracer@ysyx.org>
2024-08-11 22:49:09 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Fri Jul  5 17:56:41 PDT 2024; root:xnu-10063.141.1~2/RELEASE_ARM64_T6000 arm64 22:49  up 7 days,  7:30, 3 users, load averages: 6.85 4.70 4.05

--- npc/csrc/monitor/cpu/difftest_dut.cc
+++ npc/csrc/monitor/cpu/difftest_dut.cc
@@ -80,7 +80,7 @@ void init_difftest(char *ref_so_file, long img_size, int port)
 static void checkregs(NPCState *ref, vaddr_t pc)
 {
   bool is_same = true;
-  if ((vaddr_t)(*(ref->cpc)) != pc)
+  if ((vaddr_t)(*(ref->pc)) != pc)
   {
     printf(FMT_RED("[ERROR]") " pc is different! ref = " FMT_GREEN(FMT_WORD) ", dut = " FMT_RED(FMT_WORD) "\n",
            (vaddr_t)(*(ref->pc)), pc);
@@ -167,7 +167,7 @@ void difftest_step(vaddr_t pc)
 
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
-  checkregs(&ref_r, pc);
+  checkregs(&ref_r, *npc.cpc);
 
 #ifdef CONFIG_MEM_DIFFTEST
   if (should_diff_mem > 0)
