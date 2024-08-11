ce304dacf9aa20eba8a6d73c8a257cb8f9a599db
tracer-ysyx <tracer@ysyx.org>
2024-08-11 19:49:04 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Fri Jul  5 17:56:41 PDT 2024; root:xnu-10063.141.1~2/RELEASE_ARM64_T6000 arm64 19:49  up 7 days,  4:30, 3 users, load averages: 2.82 3.19 3.15

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -84,7 +84,7 @@ size_t ssb_start_time, ssb_end_time;
 
 __attribute__((section(".second_boot"))) void _second_stage_bootloader()
 {
-  ssb_start_time = *((uint32_t *)RTC_ADDR);
+  // ssb_start_time = *((uint32_t *)RTC_ADDR);
   if ((size_t)_text_start != (size_t)_text_load_start)
   {
     size_t text_size = _text_end - _text_start;
@@ -109,7 +109,7 @@ __attribute__((section(".second_boot"))) void _second_stage_bootloader()
     size_t data_size = _data_end - _data_start;
     memcpy(_data_start, _data_load_start, (size_t)data_size);
   }
-  ssb_end_time = *((uint32_t *)RTC_ADDR);
+  // ssb_end_time = *((uint32_t *)RTC_ADDR);
   asm volatile("mv a0, zero\nebreak");
   _trm_init();
 }
