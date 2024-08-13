6107d64994b801e1988f0f4806538e45628b5b11
tracer-ysyx <tracer@ysyx.org>
2024-08-13 14:22:56 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 14:22  up 1 day, 14:16, 2 users, load averages: 2.80 3.14 3.01

--- npc/vsrc/ysyx_pc.v
+++ npc/vsrc/ysyx_pc.v
@@ -10,12 +10,16 @@ module ysyx_pc (
     input branch_retire,
     input [DATA_W-1:0] npc_wdata,
     output wire [DATA_W-1:0] npc_o,
-    output reg valid_o, skip_o
+    output reg valid_o,
+    skip_o
 );
   parameter integer DATA_W = `ysyx_W_WIDTH;
   wire [DATA_W-1:0] npc = pc + 4;
   reg [DATA_W-1:0] pc, lpc;
-  assign npc_o = pc;
+  reg valid = 0, skip = 0;
+  assign valid_o = valid;
+  assign skip_o  = skip;
+  assign npc_o   = pc;
 
   always @(posedge clk) begin
     if (rst) begin
@@ -23,18 +27,18 @@ module ysyx_pc (
       `ysyx_DPI_C_npc_difftest_skip_ref
     end else if (prev_valid) begin
       lpc <= pc;
-      pc <= npc;
+      pc  <= npc;
       if (use_exu_npc) begin
         pc <= npc_wdata;
-        valid_o <= 1;
-        skip_o <= 0;
+        valid <= 1;
+        skip <= 0;
       end else if (branch_retire) begin
-        valid_o <= 0;
-        skip_o <= 1;
+        valid <= 0;
+        skip  <= 1;
       end
     end else begin
-      valid_o <= 0;
-      skip_o <= 0;
+      valid <= 0;
+      skip  <= 0;
     end
   end
 endmodule  //ysyx_PC
