3ebf6cb61a35617f280a78f3c3447f8aec9a086b
tracer-ysyx <tracer@ysyx.org>
2024-08-13 17:00:14 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 17:00  up 1 day, 16:53, 2 users, load averages: 4.11 4.11 3.88

--- npc/vsrc/ysyx_npc_soc.v
+++ npc/vsrc/ysyx_npc_soc.v
@@ -101,7 +101,7 @@ module ysyxSoC (
       .io_slave_rlast   (  /* unused */)
   );
 
-  ysyx_MEM_SRAM_UART perip (
+  ysyx_npc_soc perip (
       .clk(clock),
       .arburst(auto_master_out_arburst),
       .arsize(auto_master_out_arsize),
@@ -136,7 +136,7 @@ module ysyxSoC (
 endmodule  //ysyxSoC
 
 // Memory and Universal Asynchronous Receiver-Transmitter (UART)
-module ysyx_MEM_SRAM_UART (
+module ysyx_npc_soc (
     input clk,
 
     input [1:0] arburst,
