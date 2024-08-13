2b5afe73d1633e1a2f6897eef30c16eeee6d4c8e
tracer-ysyx <tracer@ysyx.org>
2024-08-13 14:29:43 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 14:29  up 1 day, 14:22, 2 users, load averages: 2.99 2.92 2.92

--- npc/vsrc/ysyx_pc.v
+++ npc/vsrc/ysyx_pc.v
@@ -17,7 +17,7 @@ module ysyx_pc (
   wire [DATA_W-1:0] npc = pc + 4;
   reg [DATA_W-1:0] pc, lpc;
   reg valid = 0, skip = 0;
-  assign valid_o = valid | prev_valid;
+  assign valid_o = valid;
   assign skip_o  = skip;
   assign npc_o   = prev_valid ? (use_exu_npc ? npc_wdata : npc) : pc;
 
