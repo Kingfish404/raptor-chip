c6a0f0a8a4083bcdb5c47d6d7de8d6a844fef2c1
tracer-ysyx <tracer@ysyx.org>
2024-08-13 14:14:07 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 14:14  up 1 day, 14:07, 2 users, load averages: 3.74 3.10 2.73

--- npc/vsrc/ysyx_ifu.v
+++ npc/vsrc/ysyx_ifu.v
@@ -67,8 +67,7 @@ module ysyx_ifu (
 
   // with l1i cache
   wire ifu_just_load = (l1i_state == 'b11 & ifu_rvalid);
-  assign inst_o = ifu_just_load & pc_ifu[2] == 1'b1
-   ? ifu_rdata : l1i[addr_idx][addr_offset];
+  assign inst_o = ifu_just_load & pc_ifu[2] == 1'b1 ? ifu_rdata : l1i[addr_idx][addr_offset];
   assign valid_o = (l1i_cache_hit & !branch_stall) | ifu_just_load;
 
   assign pc_o = pc_ifu;
