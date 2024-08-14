281d81bf5dc51fb48b4e65e3af194b9f257f82f4
tracer-ysyx <tracer@ysyx.org>
2024-08-14 16:55:18 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 16:55  up 2 days, 16:48, 3 users, load averages: 2.90 3.79 4.09

--- npc/vsrc/ysyx_exu_csr.v
+++ npc/vsrc/ysyx_exu_csr.v
@@ -23,54 +23,54 @@ module ysyx_exu_csr (
   parameter bit [7:0] BIT_W = `YSYX_W_WIDTH;
   parameter bit [BIT_W-1:0] RESET_VAL = 0;
 
-  parameter bit [REG_W-1:0] MNONE = 'h0;
-  parameter bit [REG_W-1:0] MCAUSE_IDX = 'h1;
-  parameter bit [REG_W-1:0] MEPC_IDX = 'h2;
-  parameter bit [REG_W-1:0] MTVEC_IDX = 'h3;
-  parameter bit [REG_W-1:0] MSTATUS_IDX = 'h4;
+  localparam bit [REG_W-1:0] MNONE = 'h0;
+  localparam bit [REG_W-1:0] MCAUSE = 'h1;
+  localparam bit [REG_W-1:0] MEPC = 'h2;
+  localparam bit [REG_W-1:0] MTVEC = 'h3;
+  localparam bit [REG_W-1:0] MSTATUS = 'h4;
 
   reg [BIT_W-1:0] csr[5];
   wire [REG_W-1:0] waddr_reg_1 = (
-    ({REG_W{waddr==`YSYX_CSR_MCAUSE}}) & (MCAUSE_IDX) |
-    ({REG_W{waddr==`YSYX_CSR_MEPC}}) & (MEPC_IDX) |
-    ({REG_W{waddr==`YSYX_CSR_MTVEC}}) & (MTVEC_IDX) |
-    ({REG_W{waddr==`YSYX_CSR_MSTATUS}}) & (MSTATUS_IDX) |
+    ({REG_W{waddr==`YSYX_CSR_MCAUSE}}) & (MCAUSE) |
+    ({REG_W{waddr==`YSYX_CSR_MEPC}}) & (MEPC) |
+    ({REG_W{waddr==`YSYX_CSR_MTVEC}}) & (MTVEC) |
+    ({REG_W{waddr==`YSYX_CSR_MSTATUS}}) & (MSTATUS) |
     (MNONE)
   );
   wire [REG_W-1:0] waddr_reg_2 = (
-    ({REG_W{waddr_add1==`YSYX_CSR_MCAUSE}}) & (MCAUSE_IDX) |
-    ({REG_W{waddr_add1==`YSYX_CSR_MEPC}}) & (MEPC_IDX) |
-    ({REG_W{waddr_add1==`YSYX_CSR_MTVEC}}) & (MTVEC_IDX) |
-    ({REG_W{waddr_add1==`YSYX_CSR_MSTATUS}}) & (MSTATUS_IDX) |
+    ({REG_W{waddr_add1==`YSYX_CSR_MCAUSE}}) & (MCAUSE) |
+    ({REG_W{waddr_add1==`YSYX_CSR_MEPC}}) & (MEPC) |
+    ({REG_W{waddr_add1==`YSYX_CSR_MTVEC}}) & (MTVEC) |
+    ({REG_W{waddr_add1==`YSYX_CSR_MSTATUS}}) & (MSTATUS) |
     (MNONE)
   );
 
   assign rdata_o = (
            ({BIT_W{waddr==`YSYX_CSR_MVENDORID}}) & (32'h79737978) |
            ({BIT_W{waddr==`YSYX_CSR_MARCHID}}) & (32'h15fde77) |
-           ({BIT_W{waddr==`YSYX_CSR_MSTATUS}}) & (csr[MSTATUS_IDX]) |
-           ({BIT_W{waddr==`YSYX_CSR_MCAUSE}}) & (csr[MCAUSE_IDX]) |
-           ({BIT_W{waddr==`YSYX_CSR_MEPC}}) & (csr[MEPC_IDX]) |
-           ({BIT_W{waddr==`YSYX_CSR_MTVEC}}) & (csr[MTVEC_IDX])
+           ({BIT_W{waddr==`YSYX_CSR_MSTATUS}}) & (csr[MSTATUS]) |
+           ({BIT_W{waddr==`YSYX_CSR_MCAUSE}}) & (csr[MCAUSE]) |
+           ({BIT_W{waddr==`YSYX_CSR_MEPC}}) & (csr[MEPC]) |
+           ({BIT_W{waddr==`YSYX_CSR_MTVEC}}) & (csr[MTVEC])
          );
 
-  assign mepc_o = csr[MEPC_IDX];
-  assign mtvec_o = csr[MTVEC_IDX];
+  assign mepc_o = csr[MEPC];
+  assign mtvec_o = csr[MTVEC];
 
   always @(posedge clk) begin
     if (rst) begin
-      csr[MCAUSE_IDX]  <= RESET_VAL;
-      csr[MEPC_IDX]    <= RESET_VAL;
-      csr[MTVEC_IDX]   <= RESET_VAL;
-      csr[MSTATUS_IDX] <= RESET_VAL;
+      csr[MCAUSE]  <= RESET_VAL;
+      csr[MEPC]    <= RESET_VAL;
+      csr[MTVEC]   <= RESET_VAL;
+      csr[MSTATUS] <= RESET_VAL;
     end else if (exu_valid) begin
       if (wen) begin
         csr[waddr_reg_1] <= wdata;
         csr[waddr_reg_2] <= wdata_add1;
       end
       if (ecallen) begin
-        csr[MSTATUS_IDX][`YSYX_CSR_MSTATUS_MPIE_IDX] <= csr[MSTATUS_IDX][`YSYX_CSR_MSTATUS_MIE_IDX];
-        csr[MSTATUS_IDX][`YSYX_CSR_MSTATUS_MIE_IDX] <= 1'b0;
+        csr[MSTATUS][`YSYX_CSR_MSTATUS_MPIE] <= csr[MSTATUS][`YSYX_CSR_MSTATUS_MIE];
+        csr[MSTATUS][`YSYX_CSR_MSTATUS_MIE]  <= 1'b0;
       end
     end
   end
