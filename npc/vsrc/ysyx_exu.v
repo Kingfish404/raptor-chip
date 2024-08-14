d361b1712a2dfddf55a75eca901fc037c633d8f0
tracer-ysyx <tracer@ysyx.org>
2024-08-14 19:27:06 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 19:27  up 2 days, 19:20, 3 users, load averages: 6.11 4.77 4.25

--- npc/vsrc/ysyx_exu.v
+++ npc/vsrc/ysyx_exu.v
@@ -173,8 +173,8 @@ module ysyx_exu (
       case (imm_exu[3:0])
         `YSYX_OP_SYSTEM_FUNC3: begin
           case (imm_exu[15:4])
-            `YSYX_OP_SYSTEM_ECALL:  begin use_exu_npc = 1; npc_wdata_o = mtvec; end
-            `YSYX_OP_SYSTEM_MRET:   begin use_exu_npc = 1; npc_wdata_o = mepc; end
+            `YSYX_OP_SYSTEM_ECALL:  begin npc_wdata_o = mtvec; end
+            `YSYX_OP_SYSTEM_MRET:   begin npc_wdata_o = mepc; end
             default: begin npc_wdata_o = addr_data; end
           endcase
         end
