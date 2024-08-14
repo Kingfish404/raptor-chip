691b929d9d1427879e62fbc1ee79493f965d9bfe
tracer-ysyx <tracer@ysyx.org>
2024-08-14 10:48:45 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 10:48  up 2 days, 10:41, 3 users, load averages: 2.52 2.65 2.38

--- npc/csrc/monitor/cpu/cpu_exec.cc
+++ npc/csrc/monitor/cpu/cpu_exec.cc
@@ -204,6 +204,11 @@ static void statistic()
 {
   perf();
   double time_s = g_timer / 1e6;
+  uint64_t time_clint = *(uint64_t *)&(CONCAT(VERILOG_PREFIX, __bus__DOT__clint__DOT__mtime));
+  double time_clint_us = (double)time_clint / 2;
+  double freq_clint = 1.0 / time_clint_us;
+  Log(FMT_BLUE("CLINT: %9.1f Hz, %6.3f MHz"), freq_clint, freq_clint / 1e6);
+  Log("CLINT Freq: %9.1f Hz, %6.3f MHz", freq_clint, freq_clint / 1e6);
   double frequency = pmu.active_cycle / time_s;
   Log("time: %d (ns), %d (ms)", g_timer, (int)(g_timer / 1e3));
   Log(FMT_BLUE("Simulate Freq: %9.1f Hz, %6.3f MHz"), frequency, (double)(frequency * 1.0 / 1e6));
