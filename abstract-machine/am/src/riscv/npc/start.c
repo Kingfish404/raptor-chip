6d4aed29879b3eab9cc8056803664aeda2b42047
tracer-ysyx <tracer@ysyx.org>
2024-08-21 00:12:11 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64  0:12  up 9 days, 5 mins, 4 users, load averages: 3.72 4.02 3.37

--- abstract-machine/am/src/riscv/npc/start.c
+++ abstract-machine/am/src/riscv/npc/start.c
@@ -1,5 +1,22 @@
+// .section entry, "ax"
+// .globl _start
+// .type _start, @function
 
-__attribute__((section(".text.start"))) void _start(void)
+// _start:
+//   mv s0, zero
+//   la sp, _stack_pointer
+//   jal _trm_init
+
+// __attribute__((section(".entry"))) void _start()
+// {
+// mv s0, zero
+// la sp, _stack_pointer
+// jal _trm_init
+// asm volatile("mv s0, zero");
+// asm volatile("la sp, _stack_pointer");
+// asm volatile("jal _trm_init");
+
+__attribute__((section(".entry"))) void _start(void)
 {
     asm volatile(
         "mv s0, zero\n"
