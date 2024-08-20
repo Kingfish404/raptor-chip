9039bb92adf1dfa783e8b19fcdd126430fcac1a6
tracer-ysyx <tracer@ysyx.org>
2024-08-21 00:15:57 +0800

    > sim RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64  0:15  up 9 days, 9 mins, 4 users, load averages: 2.60 3.42 3.27

--- abstract-machine/am/src/riscv/npc/start.c
+++ abstract-machine/am/src/riscv/npc/start.c
@@ -16,10 +16,10 @@
 // asm volatile("la sp, _stack_pointer");
 // asm volatile("jal _trm_init");
 
-__attribute__((section(".entry"))) void _start(void)
-{
-    asm volatile(
-        "mv s0, zero\n"
-        "la sp, _stack_pointer\n"
-        "jal _trm_init\n");
-}
+// __attribute__((section(".entry"))) void _start(void)
+// {
+//     asm volatile(
+//         "mv s0, zero\n"
+//         "la sp, _stack_pointer\n"
+//         "jal _trm_init\n");
+// }
--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -1,4 +1,4 @@
-AM_SRCS := riscv/npc/start.c \
+AM_SRCS := riscv/npc/start.S \
            riscv/npc/trm.c \
            riscv/npc/ioe.c \
            riscv/npc/timer.c \
@@ -11,7 +11,7 @@ AM_SRCS := riscv/npc/start.c \
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
 						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
-LDFLAGS   += --gc-sections -e _start
+LDFLAGS   += --gc-sections -e main
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/riscv/npc/include
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
