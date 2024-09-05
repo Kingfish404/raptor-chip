e00b2cc6444a720a4db1c1f5c628550f3d7f5b5e
tracer-ysyx <tracer@ysyx.org>
2024-09-05 10:25:59 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 10:25  up 24 days, 10:19, 3 users, load averages: 2.97 4.18 4.98

--- npc/csrc/monitor/cpu/cpu_exec.cc
+++ npc/csrc/monitor/cpu/cpu_exec.cc
@@ -157,7 +157,7 @@ static void perf_sample_per_cycle()
       pmu.l1i_cache_hit_cnt++;
       pmu.l1i_cache_hit_cycle++;
     }
-    else
+    else if (l1i_state == 0b00)
     {
       i_fetching = true;
       pmu.l1i_cache_miss_cnt++;
