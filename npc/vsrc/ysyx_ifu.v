8eb5aff66f25cc156b3de55ef1b9d47d253581cc
tracer-ysyx <tracer@ysyx.org>
2024-09-05 11:52:47 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 11:52  up 24 days, 11:46, 2 users, load averages: 9.16 6.51 6.45

--- npc/vsrc/ysyx_ifu.v
+++ npc/vsrc/ysyx_ifu.v
@@ -27,8 +27,8 @@ module ysyx_ifu (
   parameter bit [7:0] DATA_W = 32;
 
 
-  parameter bit [7:0] L1I_LINE_SIZE = 2;
   parameter bit [7:0] L1I_LINE_LEN = 1;
+  parameter bit [7:0] L1I_LINE_SIZE = 2 ** L1I_LINE_LEN;
   parameter bit [7:0] L1I_LEN = 2;
   parameter bit [7:0] L1I_SIZE = 2 ** L1I_LEN;
 
