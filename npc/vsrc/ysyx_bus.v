17b0fce14517b2d72de38515426bad211af9779e
tracer-ysyx <tracer@ysyx.org>
2024-09-07 01:26:51 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64  1:26  up 26 days,  1:20, 5 users, load averages: 5.35 5.15 4.87

--- npc/vsrc/ysyx_bus.v
+++ npc/vsrc/ysyx_bus.v
@@ -78,9 +78,9 @@ module ysyx_bus (
 
   // typedef enum [2:0] {IF_A, IF_D, LS_A, LS_D_R} state_t;
   //                      000,  001,  010,    011
-  parameter bit [3:0] IF_A = 3'b000, IF_D = 3'b001;
-  parameter bit [3:0] LS_A = 3'b010, LS_D_R = 3'b011;
-  parameter bit [2:0] LS_S_A = 3'b001, LS_S_W = 3'b010, LS_S_B = 3'b100;
+  parameter bit [3:0] IF_A = 'b000, IF_D = 'b001;
+  parameter bit [3:0] LS_A = 'b010, LS_D_R = 'b011;
+  parameter bit [2:0] LS_S_A = 'b001, LS_S_W = 'b010, LS_S_B = 'b100;
 
   reg [3:0] state;
   reg first = 1;
