924734b7a5618d94e384a5847057c8f4f6b5cdab
tracer-ysyx <tracer@ysyx.org>
2024-09-07 11:38:39 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 11:38  up 26 days, 11:31, 5 users, load averages: 5.05 4.21 4.99

--- npc/csrc/monitor/cpu/pmu.cc
+++ npc/csrc/monitor/cpu/pmu.cc
@@ -25,25 +25,24 @@ void perf()
   printf("| %8s,  %% | %8s,  %% | %8s,  %% |\n",
          "IFU", "LSU", "EXU");
   printf("| %8lld,%3.0f | %8lld,%3.0f | %8lld,%3.0f |\n",
-         (long long)pmu.ifu_fetch_stall_cycle, percentage(pmu.ifu_fetch_stall_cycle, pmu.active_cycle),
-         (long long)pmu.lsu_stall_cycle, percentage(pmu.lsu_stall_cycle, pmu.active_cycle),
-         (long long)pmu.exu_alu_cnt, percentage(pmu.exu_alu_cnt, pmu.instr_cnt));
+         pmu.ifu_fetch_stall_cycle, percentage(pmu.ifu_fetch_stall_cycle, pmu.active_cycle),
+         pmu.lsu_stall_cycle, percentage(pmu.lsu_stall_cycle, pmu.active_cycle),
+         pmu.exu_alu_cnt, percentage(pmu.exu_alu_cnt, pmu.instr_cnt));
   printf("| %6s, %% | %6s, %% | %6s, %% | %6s, %% | %3s, %% | %5s, %% | %6s,  %% | %6s,  %% |\n",
          "LD", "ST", "ALU", "BR", "CSR", "OTH", "JAL", "JALR");
-  printf(
-      "| %6lld,%2.0f | %6lld,%2.0f | %6lld,%2.0f "
-      "| %6lld,%2.0f | %3lld,%2.0f | %5lld,%2.0f "
-      "| %6lld,%3.0f | %6lld,%3.0f |\n",
-      (long long)pmu.ld_inst_cnt, percentage(pmu.ld_inst_cnt, pmu.instr_cnt),
-      (long long)pmu.st_inst_cnt, percentage(pmu.st_inst_cnt, pmu.instr_cnt),
-      (long long)pmu.alu_inst_cnt, percentage(pmu.alu_inst_cnt, pmu.instr_cnt),
+  printf("| %6lld,%2.0f | %6lld,%2.0f | %6lld,%2.0f "
+         "| %6lld,%2.0f | %3lld,%2.0f | %5lld,%2.0f "
+         "| %6lld,%3.0f | %6lld,%3.0f |\n",
+         (long long)pmu.ld_inst_cnt, percentage(pmu.ld_inst_cnt, pmu.instr_cnt),
+         (long long)pmu.st_inst_cnt, percentage(pmu.st_inst_cnt, pmu.instr_cnt),
+         (long long)pmu.alu_inst_cnt, percentage(pmu.alu_inst_cnt, pmu.instr_cnt),
 
-      (long long)pmu.b_inst_cnt, percentage(pmu.b_inst_cnt, pmu.instr_cnt),
-      (long long)pmu.csr_inst_cnt, percentage(pmu.csr_inst_cnt, pmu.instr_cnt),
-      (long long)pmu.other_inst_cnt, percentage(pmu.other_inst_cnt, pmu.instr_cnt),
+         (long long)pmu.b_inst_cnt, percentage(pmu.b_inst_cnt, pmu.instr_cnt),
+         (long long)pmu.csr_inst_cnt, percentage(pmu.csr_inst_cnt, pmu.instr_cnt),
+         (long long)pmu.other_inst_cnt, percentage(pmu.other_inst_cnt, pmu.instr_cnt),
 
-      pmu.jal_inst_cnt, percentage(pmu.jal_inst_cnt, pmu.instr_cnt),
-      pmu.jalr_inst_cnt, percentage(pmu.jalr_inst_cnt, pmu.instr_cnt));
+         pmu.jal_inst_cnt, percentage(pmu.jal_inst_cnt, pmu.instr_cnt),
+         pmu.jalr_inst_cnt, percentage(pmu.jalr_inst_cnt, pmu.instr_cnt));
   printf("======== TOP DOWN Analysis ========\n");
   printf("| %8s,  %% | %8s,  %% | %8s,  %% | %8s,  %% | %8s,  %% |\n",
          "IFU", "LSU", "EXU", "LD", "ST");
