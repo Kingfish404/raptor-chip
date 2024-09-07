8f715826b1e9a9794151cab587f251b9dd948918
tracer-ysyx <tracer@ysyx.org>
2024-09-07 11:30:00 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 11:30  up 26 days, 11:23, 5 users, load averages: 5.69 5.91 6.10

--- npc/csrc/monitor/cpu/pmu.cc
+++ npc/csrc/monitor/cpu/pmu.cc
@@ -24,17 +24,22 @@ void perf()
   Log(FMT_BLUE("Cycle: %llu, #Inst: %lld, IPC: %.3f"), pmu.active_cycle, pmu.instr_cnt, (1.0 * pmu.instr_cnt / pmu.active_cycle));
   printf("| %8s,  %% | %8s,  %% | %8s,  %% | %6s, %% | %6s, %% | %6s, %% | %6s, %% | %6s, %% | %6s, %% | %3s, %% | %5s, %% |\n",
          "IFU", "LSU", "EXU", "LD", "ST", "ALU", "BR", "JAL", "JALR", "CSR", "OTH");
-  printf("| %8lld,%3.0f | %8lld,%3.0f | %8lld,%3.0f | %6lld,%2.0f "
-         "| %6lld,%2.0f | %6lld,%2.0f | %6lld,%2.0f | %3lld,%2.0f | %5lld,%2.0f |\n",
+  printf("| %8lld,%3.0f | %8lld,%3.0f | %8lld,%3.0f "
+         "| %6lld,%2.0f | %6lld,%2.0f | %6lld,%2.0f "
+         "| %6lld,%2.0f | %6lld,%2.0f | %6lld,%2.0f "
+         "| %3lld,%2.0f | %5lld,%2.0f |\n",
          (long long)pmu.ifu_fetch_stall_cycle, percentage(pmu.ifu_fetch_stall_cycle, pmu.active_cycle),
          (long long)pmu.lsu_stall_cycle, percentage(pmu.lsu_stall_cycle, pmu.active_cycle),
          (long long)pmu.exu_alu_cnt, percentage(pmu.exu_alu_cnt, pmu.instr_cnt),
+
          (long long)pmu.ld_inst_cnt, percentage(pmu.ld_inst_cnt, pmu.instr_cnt),
          (long long)pmu.st_inst_cnt, percentage(pmu.st_inst_cnt, pmu.instr_cnt),
          (long long)pmu.alu_inst_cnt, percentage(pmu.alu_inst_cnt, pmu.instr_cnt),
+
          (long long)pmu.b_inst_cnt, percentage(pmu.b_inst_cnt, pmu.instr_cnt),
          (long long)pmu.jal_inst_cnt, percentage(pmu.jal_inst_cnt, pmu.instr_cnt),
          (long long)pmu.jalr_inst_cnt, percentage(pmu.jalr_inst_cnt, pmu.instr_cnt),
+
          (long long)pmu.csr_inst_cnt, percentage(pmu.csr_inst_cnt, pmu.instr_cnt),
          (long long)pmu.other_inst_cnt, percentage(pmu.other_inst_cnt, pmu.instr_cnt));
   printf("======== TOP DOWN Analysis ========\n");
