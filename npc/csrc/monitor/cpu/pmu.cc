43dc613417ec94a6ebed8ccd45da0c627439ae26
tracer-ysyx <tracer@ysyx.org>
2024-09-15 21:57:59 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 21:57  up 34 days, 21:51, 5 users, load averages: 9.41 8.71 8.70

--- npc/csrc/monitor/cpu/pmu.cc
+++ npc/csrc/monitor/cpu/pmu.cc
@@ -191,9 +191,9 @@ void perf()
   Log(FMT_BLUE("IFU Avg Cycle: %2.1f, LSU Avg Cycle: %2.1f"),
       (1.0 * pmu.ifu_fetch_stall_cycle) / (pmu.ifu_fetch_cnt + 1),
       (1.0 * pmu.lsu_stall_cycle) / (pmu.lsu_load_cnt + 1));
-  Log(FMT_BLUE("BPU Success: %lld, Fail: %lld, Rate: %2.1f%%"),
-      pmu.bpu_success_cnt, pmu.bpu_fail_cnt,
-      percentage(pmu.bpu_success_cnt, pmu.bpu_success_cnt + pmu.bpu_fail_cnt));
+  printf("BPU Success: %lld, Fail: %lld, Rate: %2.1f%%",
+         pmu.bpu_success_cnt, pmu.bpu_fail_cnt,
+         percentage(pmu.bpu_success_cnt, pmu.bpu_success_cnt + pmu.bpu_fail_cnt));
   printf("ifu_hazard_cycle: %8lld,%3.0f%% (branch + load instruction (%8lld,%3.0f%%))\n",
          pmu.ifu_hazard_cycle, percentage(pmu.ifu_hazard_cycle, pmu.active_cycle),
          pmu.ifu_lsu_hazard_cycle, percentage(pmu.ifu_lsu_hazard_cycle, pmu.active_cycle));
