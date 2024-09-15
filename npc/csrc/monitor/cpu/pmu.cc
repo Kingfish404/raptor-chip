360c8c91daed1aaea476ddfd752930bdf8b3f739
tracer-ysyx <tracer@ysyx.org>
2024-09-15 15:32:14 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 15:32  up 34 days, 15:25, 5 users, load averages: 8.55 6.60 7.55

--- npc/csrc/monitor/cpu/pmu.cc
+++ npc/csrc/monitor/cpu/pmu.cc
@@ -146,8 +146,8 @@ void perf()
 
   uint64_t time_clint = *(uint64_t *)&(CONCAT(VERILOG_PREFIX, bus__DOT__clint__DOT__mtime));
   uint64_t time_clint_us = time_clint / 2;
-  Log("#Inst: %lld, Cycle: %llu, "
-      "" FMT_BLUE("IPC: %2.3f ") ", CLINT time: %lld (us), " FMT_BLUE(" %2.3f MIPS"),
+  Log("#Inst: %lld, cycle: %llu, "
+      "" FMT_BLUE("IPC: %2.3f ") ", CLINT: %lld (us), " FMT_BLUE(" %2.3f MIPS"),
       pmu.instr_cnt, pmu.active_cycle, (1.0 * pmu.instr_cnt / pmu.active_cycle),
       (time_clint_us), (double)((pmu.instr_cnt / 1e6) / (time_clint_us / 1e6)));
   return;
