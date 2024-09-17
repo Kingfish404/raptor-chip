87094bf1b51bce1658680c431115a68c0ce8e91e
tracer-ysyx <tracer@ysyx.org>
2024-09-17 21:39:46 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 23.6.0 Darwin Kernel Version 23.6.0: Mon Jul 29 21:14:30 PDT 2024; root:xnu-10063.141.2~1/RELEASE_ARM64_T6000 arm64 21:39  up 36 days, 21:32, 5 users, load averages: 7.53 6.09 5.65

--- npc/vsrc/ysyx_ifu.v
+++ npc/vsrc/ysyx_ifu.v
@@ -179,23 +179,23 @@ module ysyx_ifu (
     end
   end
 
-  wire l1i_valid1;
-  wire [DATA_W-1:0] inst_l1i;
-  ysyx_ifu_l1i ifu_l1i (
-      .clk(clk),
-      .rst(rst),
+  // wire l1i_valid1;
+  // wire [DATA_W-1:0] inst_l1i;
+  // ysyx_ifu_l1i ifu_l1i (
+  //     .clk(clk),
+  //     .rst(rst),
 
-      .ifu_addr(pc_ifu),
+  //     .ifu_addr(pc_ifu),
 
-      .ifu_araddr_o(ifu_araddr_o),
-      .ifu_arvalid_o(ifu_arvalid_o),
-      .ifu_required_o(ifu_required_o),
-      .ifu_rdata(ifu_rdata),
-      .ifu_rvalid(ifu_rvalid),
+  //     .ifu_araddr_o(ifu_araddr_o),
+  //     .ifu_arvalid_o(ifu_arvalid_o),
+  //     .ifu_required_o(ifu_required_o),
+  //     .ifu_rdata(ifu_rdata),
+  //     .ifu_rvalid(ifu_rvalid),
 
-      .inst_o(inst_l1i),
-      .valid_o(l1i_valid1)
-  );
+  //     .inst_o (inst_l1i),
+  //     .valid_o(l1i_valid1)
+  // );
 
   always @(posedge clk) begin
     if (rst) begin
@@ -240,28 +240,80 @@ module ysyx_ifu (
 endmodule  // ysyx_IFU
 
 
-module ysyx_ifu_l1i (
-    input clk,
-    input rst,
+// module ysyx_ifu_l1i (
+//     input clk,
+//     input rst,
 
-    // from ifu
-    input [DATA_W-1:0] ifu_addr,
+//     // from ifu
+//     input [DATA_W-1:0] ifu_addr,
 
-    // for bus
-    output [DATA_W-1:0] ifu_araddr_o,
-    output ifu_arvalid_o,
-    output ifu_required_o,
-    input [DATA_W-1:0] ifu_rdata,
-    input ifu_rvalid,
+//     // for bus
+//     output [DATA_W-1:0] ifu_araddr_o,
+//     output ifu_arvalid_o,
+//     output ifu_required_o,
+//     input [DATA_W-1:0] ifu_rdata,
+//     input ifu_rvalid,
 
-    // to ifu
-    output reg [DATA_W-1:0] inst_o,
+//     // to ifu
+//     output reg [DATA_W-1:0] inst_o,
 
-    output reg valid_o
-);
-  parameter bit [7:0] DATA_W = 32;
+//     output reg valid_o
+// );
+//   parameter bit [7:0] DATA_W = 32;
 
-  assign inst_o = ifu_addr;
-  assign valid_o = 1;
+//   parameter bit [7:0] L1I_LINE_LEN = 1;
+//   parameter bit [7:0] L1I_LINE_SIZE = 2 ** L1I_LINE_LEN;
+//   parameter bit [7:0] L1I_LEN = 2;
+//   parameter bit [7:0] L1I_SIZE = 2 ** L1I_LEN;
 
-endmodule  // ysyx_IFU_L1I
+//   assign inst_o  = ifu_addr;
+//   assign valid_o = 1;
+
+//   reg [32-1:0] l1i[L1I_SIZE][L1I_LINE_SIZE];
+//   reg [L1I_SIZE-1:0] l1i_valid = 0;
+//   reg [32-L1I_LEN-L1I_LINE_LEN-2-1:0] l1i_tag[L1I_SIZE][L1I_LINE_SIZE];
+//   reg [2:0] l1i_state = 0;
+
+
+//   always @(posedge clk) begin
+//     if (rst) begin
+//       l1i_state <= 'b000;
+//       l1i_valid <= 0;
+//     end else begin
+//       case (l1i_state)
+//         'b000:
+//         if (ifu_arvalid_o & !bad_speculation) begin
+//           l1i_state <= 'b001;
+//         end
+//         'b001:
+//         if (ifu_rvalid & !l1i_cache_hit) begin
+//           if (ifu_sdram_arburst) begin
+//             l1i_state <= 'b011;
+//           end else begin
+//             l1i_state <= 'b010;
+//           end
+//           l1i[addr_idx][0] <= ifu_rdata;
+//           l1i_tag[addr_idx][0] <= addr_tag;
+//         end
+//         'b010: begin
+//           l1i_state <= 'b011;
+//         end
+//         'b011: begin
+//           if (ifu_rvalid) begin
+//             l1i_state <= 'b100;
+//             l1i[addr_idx][1] <= ifu_rdata;
+//             l1i_tag[addr_idx][1] <= addr_tag;
+//             l1i_valid[addr_idx] <= 1'b1;
+//           end
+//         end
+//         'b100: begin
+//           l1i_state <= 'b000;
+//         end
+//         default begin
+//           l1i_state <= 'b000;
+//         end
+//       endcase
+//     end
+//   end
+
+// endmodule  // ysyx_IFU_L1I
