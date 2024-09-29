18255fa0d7a9c5323292490400dac0dfa9dbff04
tracer-ysyx <tracer@ysyx.org>
2024-09-29 16:34:27 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 24.0.0 Darwin Kernel Version 24.0.0: Mon Aug 12 20:51:54 PDT 2024; root:xnu-11215.1.10~2/RELEASE_ARM64_T6000 arm64 16:34  up 10 days, 22:41, 7 users, load averages: 5.95 5.48 6.05

--- npc/vsrc/ysyx_exu_csr.sv
+++ npc/vsrc/ysyx_exu_csr.sv
@@ -64,8 +64,8 @@ module ysyx_exu_csr (
       csr[MSTATUS] <= RESET_VAL;
     end else begin
       if (wen & exu_valid) begin
-        $display("csr[%0d] <= %0d", waddr_reg_1, wdata);
-        $display("csr[%0d] <= %0d", waddr_reg_2, wdata_add1);
+        $display("1 csr[%0d] <= %0d", waddr_reg_1, wdata);
+        $display("2 csr[%0d] <= %0d", waddr_reg_2, wdata_add1);
         csr[waddr_reg_1] <= wdata;
         csr[waddr_reg_2] <= wdata_add1;
       end
