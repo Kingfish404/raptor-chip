ebdd18e03f52fdc7b897330bdf7fdf1a5b71b23a
tracer-ysyx <tracer@ysyx.org>
2024-09-29 20:26:00 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 24.0.0 Darwin Kernel Version 24.0.0: Mon Aug 12 20:51:54 PDT 2024; root:xnu-11215.1.10~2/RELEASE_ARM64_T6000 arm64 20:26  up 11 days,  2:32, 7 users, load averages: 13.97 12.39 11.64

--- npc/vsrc/ysyx_lsu.sv
+++ npc/vsrc/ysyx_lsu.sv
@@ -4,40 +4,40 @@ module ysyx_lsu (
     input rst,
 
     // from exu
-    input [ADDR_W-1:0] addr,
+    input [BIT_W-1:0] addr,
     input ren,
     input wen,
     lsu_avalid,
     input [3:0] alu_op,
-    input [DATA_W-1:0] wdata,
+    input [BIT_W-1:0] wdata,
     // to exu
-    output [DATA_W-1:0] rdata_o,
+    output [BIT_W-1:0] rdata_o,
     output rvalid_o,
     output wready_o,
 
     // to bus load
-    output [DATA_W-1:0] lsu_araddr_o,
+    output [BIT_W-1:0] lsu_araddr_o,
     output lsu_arvalid_o,
     output [7:0] lsu_rstrb_o,
     // from bus load
-    input [DATA_W-1:0] lsu_rdata,
+    input [BIT_W-1:0] lsu_rdata,
     input lsu_rvalid,
 
     // to bus store
-    output [DATA_W-1:0] lsu_awaddr_o,
+    output [BIT_W-1:0] lsu_awaddr_o,
     output lsu_awvalid_o,
-    output [DATA_W-1:0] lsu_wdata_o,
+    output [BIT_W-1:0] lsu_wdata_o,
     output [7:0] lsu_wstrb_o,
     output lsu_wvalid_o,
     // from bus store
     input reg lsu_wready
 );
-  parameter bit [7:0] ADDR_W = 32, DATA_W = 32;
+  parameter bit [7:0] BIT_W = 32;
 
-  reg [ADDR_W-1:0] lsu_araddr;
+  reg [BIT_W-1:0] lsu_araddr;
   reg valid_r;
 
-  wire [DATA_W-1:0] rdata, rdata_unalign;
+  wire [BIT_W-1:0] rdata, rdata_unalign;
   wire [7:0] wstrb, rstrb;
 
   assign lsu_araddr_o = lsu_araddr;
@@ -71,7 +71,7 @@ module ysyx_lsu (
   reg [32-L1D_LEN-2-1:0] l1d_tag[L1D_SIZE];
 
   wire arvalid;
-  wire [32-L1D_LEN-2-1:0] addr_tag = lsu_araddr_o[ADDR_W-1:L1D_LEN+2];
+  wire [32-L1D_LEN-2-1:0] addr_tag = lsu_araddr_o[BIT_W-1:L1D_LEN+2];
   wire [L1D_LEN-1:0] addr_idx = lsu_araddr_o[L1D_LEN+2-1:0+2];
   wire l1d_cache_hit = (
          ren & lsu_avalid & 1 &
@@ -83,7 +83,7 @@ module ysyx_lsu (
          (0)
        );
 
-  wire [32-L1D_LEN-2-1:0] waddr_tag = lsu_awaddr_o[ADDR_W-1:L1D_LEN+2];
+  wire [32-L1D_LEN-2-1:0] waddr_tag = lsu_awaddr_o[BIT_W-1:L1D_LEN+2];
   wire [L1D_LEN-1:0] waddr_idx = lsu_awaddr_o[L1D_LEN+2-1:0+2];
   wire l1d_cache_hit_w = (
          wen & lsu_avalid &
@@ -105,19 +105,19 @@ module ysyx_lsu (
 
   wire [1:0] araddr_lo = lsu_araddr_o[1:0];
   assign rdata = (
-           ({DATA_W{araddr_lo == 2'b00}} & rdata_unalign) |
-           ({DATA_W{araddr_lo == 2'b01}} & {{8'b0}, {rdata_unalign[31:8]}}) |
-           ({DATA_W{araddr_lo == 2'b10}} & {{16'b0}, {rdata_unalign[31:16]}}) |
-           ({DATA_W{araddr_lo == 2'b11}} & {{24'b0}, {rdata_unalign[31:24]}}) |
+           ({BIT_W{araddr_lo == 2'b00}} & rdata_unalign) |
+           ({BIT_W{araddr_lo == 2'b01}} & {{8'b0}, {rdata_unalign[31:8]}}) |
+           ({BIT_W{araddr_lo == 2'b10}} & {{16'b0}, {rdata_unalign[31:16]}}) |
+           ({BIT_W{araddr_lo == 2'b11}} & {{24'b0}, {rdata_unalign[31:24]}}) |
            (0)
          );
   assign rdata_o = (
-           ({DATA_W{alu_op == `YSYX_ALU_OP_LB}} & (rdata[7] ? rdata | 'hffffff00 : rdata & 'hff)) |
-           ({DATA_W{alu_op == `YSYX_ALU_OP_LBU}} & rdata & 'hff) |
-           ({DATA_W{alu_op == `YSYX_ALU_OP_LH}} &
+           ({BIT_W{alu_op == `YSYX_ALU_OP_LB}} & (rdata[7] ? rdata | 'hffffff00 : rdata & 'hff)) |
+           ({BIT_W{alu_op == `YSYX_ALU_OP_LBU}} & rdata & 'hff) |
+           ({BIT_W{alu_op == `YSYX_ALU_OP_LH}} &
               (rdata[15] ? rdata | 'hffff0000 : rdata & 'hffff)) |
-           ({DATA_W{alu_op == `YSYX_ALU_OP_LHU}} & rdata & 'hffff) |
-           ({DATA_W{alu_op == `YSYX_ALU_OP_LW}} & rdata)
+           ({BIT_W{alu_op == `YSYX_ALU_OP_LHU}} & rdata & 'hffff) |
+           ({BIT_W{alu_op == `YSYX_ALU_OP_LW}} & rdata)
          );
   assign lsu_araddr = addr;
   always @(posedge clk) begin
