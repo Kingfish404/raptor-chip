fe3c985a9e187b46350be63e083605c7c0508526
tracer-ysyx <tracer@ysyx.org>
2024-10-04 00:18:16 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 24.0.0 Darwin Kernel Version 24.0.0: Mon Aug 12 20:51:54 PDT 2024; root:xnu-11215.1.10~2/RELEASE_ARM64_T6000 arm64  0:18  up 10:15, 1 user, load averages: 5.57 4.63 3.92

--- npc/vsrc/ysyx_ifu.sv
+++ npc/vsrc/ysyx_ifu.sv
@@ -77,8 +77,22 @@ module ysyx_ifu (
   //     end
   //   endcase
   // end
-  assign state = (state == `YSYX_IDLE & valid_o) ? `YSYX_WAIT_READY :
-    (state == `YSYX_WAIT_READY & next_ready) ? `YSYX_IDLE : 0;
+  // assign state = (state == `YSYX_IDLE & valid_o) ? `YSYX_WAIT_READY :
+  //   (state == `YSYX_WAIT_READY & next_ready) ? `YSYX_IDLE : 0;
+  // State register update (on rising edge of the clock or reset)
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+      state <= `YSYX_IDLE;  // Reset state to IDLE
+    end else begin
+      state <= next_state;  // Update state to the next state
+    end
+  end
+  wire next_state;
+  // Combinational logic to calculate next state
+  assign next_state = (state == `YSYX_IDLE) ?
+                    (valid_o ? `YSYX_WAIT_READY : `YSYX_IDLE) :
+                    (state == `YSYX_WAIT_READY) ?
+                    (next_ready ? `YSYX_IDLE : `YSYX_WAIT_READY) : state;
   always @(posedge clk) begin
     if (rst) begin
       pc_ifu <= `YSYX_PC_INIT;
