b4d9777c872827ccab06fe9833513395092b071d
tracer-ysyx <tracer@ysyx.org>
2024-10-04 17:47:11 +0800

    > build RTL ysyx_2022111024 金宇 Darwin Yus-MacBook-Pro.local 24.0.0 Darwin Kernel Version 24.0.0: Mon Aug 12 20:51:54 PDT 2024; root:xnu-11215.1.10~2/RELEASE_ARM64_T6000 arm64 17:47  up 1 day,  3:44, 2 users, load averages: 4.89 5.50 5.35

--- npc/vsrc/ysyx.sv
+++ npc/vsrc/ysyx.sv
@@ -254,7 +254,7 @@ module ysyx (
       .pc(pc_wbu),
       .pc_change(pc_change),
       .pc_retire(pc_retire),
-      .exu_valid(exu_valid),
+      .exu_valid(branch_retire),
       .speculation_o(speculation_ifu),
       .bad_speculation_o(bad_speculation),
       .good_speculation_o(good_speculation),
